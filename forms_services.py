aa49d810a40921a09b1f2b677f26d53997721649
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2016-04-21 16:03:16 +0000

    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol_services@1099 216424d3-d1a8-4ca4-951f-a609719bba38

---
+++ forms_services.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+
+'''
+    gvSIG Online.
+    Copyright (C) 2007-2015 gvSIG Association.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+'''
+@author: César Martinez <cmartinez@scolab.es>
+'''
+from models import Workspace, Datastore, Layer, LayerGroup
+from django.utils.translation import ugettext as _
+from backend_mapservice import backend
+from django import forms
+import json
+
+class WorkspaceForm(forms.Form):   
+    name = forms.CharField(required=True, max_length=250, widget=forms.TextInput(attrs={'class' : 'form-control', 'tabindex': '1'}))
+    description = forms.CharField(required=False, max_length=500, widget=forms.TextInput(attrs={'class' : 'form-control', 'tabindex': '2'}))
+    uri = forms.CharField(required=True, max_length=500, widget=forms.TextInput(attrs={'class' : 'form-control', 'tabindex': '3'}))
+    wms_endpoint = forms.CharField(required=False, max_length=500, widget=forms.TextInput(attrs={'class' : 'form-control', 'tabindex': '4'}))
+    wfs_endpoint = forms.CharField(required=False, max_length=500, widget=forms.TextInput(attrs={'class' : 'form-control', 'tabindex': '5'}))
+    wcs_endpoint = forms.CharField(required=False, max_length=500, widget=forms.TextInput(attrs={'class' : 'form-control', 'tabindex': '6'}))
+    cache_endpoint = forms.CharField(required=False, max_length=500, widget=forms.TextInput(attrs={'class' : 'form-control', 'tabindex': '7'}))
+
+class DatastoreForm(forms.Form):
+    workspace = forms.ModelChoiceField(required=True, queryset=Workspace.objects.all(), widget=forms.Select(attrs={'class':'form-control'}))
+    type = forms.ChoiceField(choices=backend.getSupportedTypes(), required=True, widget=forms.Select(attrs={'class':'form-control'}))  
+    name = forms.CharField(required=True, max_length=250, widget=forms.TextInput(attrs={'class': 'form-control', 'tabindex': '2'}))
+    description = forms.CharField(required=False, max_length=500, widget=forms.TextInput(attrs={'class' : 'form-control', 'tabindex': '3'}))
+    connection_params = forms.CharField(required=True, widget=forms.Textarea(attrs={'class': 'form-control', 'tabindex': '4'}))
+
+    def __init__(self, session, *args, **kwargs):
+        self.session = session
+        super(DatastoreForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        cleaned_data = super(DatastoreForm, self).clean()
+        workspace = cleaned_data.get("workspace")
+        name = cleaned_data.get("name")
+        connection_params = cleaned_data.get("connection_params")
+
+        if name and workspace:
+            if backend.datastore_exists(workspace.name, name, self.session):
+                self.add_error('name', _("Datastore already exists")) 
+        
+        if connection_params:
+            try:
+                json.loads(connection_params) 
+            except:
+                self.add_error('connection_params', _("Error: Invalid JSON format"))
+        return cleaned_data
+
+class DatastoreUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Datastore
+        fields = ['type', 'name', 'description', 'connection_params']
+    type = forms.CharField(required=True, max_length=250, widget=forms.TextInput(attrs={'class':'form-control', 'readonly': 'true'}))
+    name = forms.CharField(required=True, max_length=250, widget=forms.TextInput(attrs={'class':'form-control', 'readonly': 'true'}))   
+    description = forms.CharField(required=False, max_length=500, widget=forms.TextInput(attrs={'class':'form-control', 'tabindex': '1'}))
+    connection_params = forms.CharField(required=True, widget=forms.Textarea(attrs={'class':'form-control', 'tabindex': '2'}))
+    
+    def clean(self):
+        cleaned_data = super(DatastoreUpdateForm, self).clean()
+        connection_params = cleaned_data.get("connection_params") 
+        
+        if connection_params:
+            try:
+                cleaned_data['connection_params_json'] = json.loads(connection_params) 
+            except:
+                self.add_error('connection_params', _("Error: Invalid JSON format"))
+        return cleaned_data
+
+
+class LayerForm(forms.ModelForm):
+    class Meta:
+        model = Layer
+        fields = ['datastore', 'name', 'title', 'layer_group', 'visible', 'queryable']
+    datastore = forms.ModelChoiceField(required=True, queryset=Datastore.objects.all(), widget=forms.Select(attrs={'class' : 'validate'}))
+    name = forms.CharField(required=True, widget=forms.Select(attrs={'class' : 'validate'}))
+    title = forms.CharField(required=True, max_length=150, widget=forms.TextInput(attrs={'class' : 'validate'}))
+    layer_group = forms.ModelChoiceField(required=True, queryset=LayerGroup.objects.all(), widget=forms.Select(attrs={'class' : 'validate'}))
+    #visible = forms.BooleanField(initial=True, widget=forms.CheckboxInput(attrs={'class' : 'validate filled-in'}))
+    #queryable = forms.BooleanField(initial=True, widget=forms.CheckboxInput(attrs={'class' : 'validate filled-in'}))
+
+class LayerUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Layer
+        fields = ['datastore', 'name', 'title', 'layer_group', 'visible', 'queryable']
+    datastore = forms.ModelChoiceField(required=True, queryset=Datastore.objects.all(), widget=forms.Select(attrs={'class' : 'validate', 'readonly': 'true'}))
+    name = forms.CharField(required=True, max_length=100, widget=forms.TextInput(attrs={'class' : 'validate', 'readonly': 'true'}))
+    title = forms.CharField(required=True, max_length=150, widget=forms.TextInput(attrs={'class' : 'validate'}))
+    layer_group = forms.ModelChoiceField(required=True, queryset=LayerGroup.objects.all(), widget=forms.Select(attrs={'class' : 'validate'}))
+    #visible = forms.BooleanField(initial=True, widget=forms.CheckboxInput(attrs={'class' : 'validate filled-in'}))
+    #queryable = forms.BooleanField(initial=True, widget=forms.CheckboxInput(attrs={'class' : 'validate filled-in'}))
+    #cached = forms.BooleanField(initial=True, widget=forms.CheckboxInput(attrs={'class' : 'validate filled-in'}))
+
+class LayerUploadTypeForm(forms.ModelForm):
+    class Meta:
+        model = Datastore
+        fields = ['type']
+    type = forms.ModelChoiceField(required=True, queryset=Workspace.objects.all())
--- urls.py
+++ urls.py
@@ -1,6 +1,29 @@
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^login_user/$', 'gvsigol_auth.views.login_user', name='login_user'), 
-    url(r'^logout_user/$', 'gvsigol_auth.views.logout_user', name='logout_user'),
+    url(r'^workspace_list/$', 'gvsigol_services.views.workspace_list', name='workspace_list'),
+    url(r'^workspace_add/$', 'gvsigol_services.views.workspace_add', name='workspace_add'),
+    url(r'^workspace_import/$', 'gvsigol_services.views.workspace_import', name='workspace_import'),
+    url(r'^workspace_delete/(?P<wsid>\d+)/$', 'gvsigol_services.views.workspace_delete', name='workspace_delete'),
+    url(r'^datastore_list/$', 'gvsigol_services.views.datastore_list', name='datastore_list'),
+    url(r'^datastore_add/$', 'gvsigol_services.views.datastore_add', name='datastore_add'),
+    url(r'^datastore_update/(?P<datastore_id>[0-9]+)/$', 'gvsigol_services.views.datastore_update', name='datastore_update'),
+    url(r'^datastore_delete/(?P<dsid>\d+)/$', 'gvsigol_services.views.datastore_delete', name='datastore_delete'),
+    url(r'^backend_resource_list_available/$', 'gvsigol_services.views.backend_resource_list_available', name='backend_resource_list_available'),
+    url(r'^layer_list/$', 'gvsigol_services.views.layer_list', name='layer_list'),
+    url(r'^layer_add/$', 'gvsigol_services.views.layer_add', name='layer_add'),
+    url(r'^layer_permissions/(?P<layer_id>[0-9]+)/$', 'gvsigol_services.views.layer_permissions_update', name='layer_permissions_update'),
+    url(r'^get_resources_from_workspace/$', 'gvsigol_services.views.get_resources_from_workspace', name='get_resources_from_workspace'),
+    url(r'^layer_update/(?P<layer_id>[0-9]+)/$', 'gvsigol_services.views.layer_update', name='layer_update'),
+    url(r'^layer_delete/(?P<layer_id>[0-9]+)/$', 'gvsigol_services.views.layer_delete', name='layer_delete'),
+    url(r'^cache_clear/(?P<layer_id>[0-9]+)/$', 'gvsigol_services.views.cache_clear', name='cache_clear'),
+    url(r'^layergroup_cache_clear/(?P<layergroup_id>[0-9]+)/$', 'gvsigol_services.views.layergroup_cache_clear', name='layergroup_cache_clear'),
+    url(r'^layer_upload/$', 'gvsigol_services.views.layer_upload', name='layer_upload'),
+    url(r'^layer_create/$', 'gvsigol_services.views.layer_create', name='layer_create'),
+    url(r'^get_geom_tables/(?P<datastore_id>[0-9]+)/$', 'gvsigol_services.views.get_geom_tables', name='geom_tables'),
+    url(r'^layergroup_list/$', 'gvsigol_services.views.layergroup_list', name='layergroup_list'),
+    url(r'^layergroup_add/$', 'gvsigol_services.views.layergroup_add', name='layergroup_add'),
+    url(r'^layergroup_delete/(?P<lgid>[0-9]+)/$', 'gvsigol_services.views.layergroup_delete', name='layergroup_delete'),
+    url(r'^layergroup_update/(?P<lgid>[0-9]+)/$', 'gvsigol_services.views.layergroup_update', name='layergroup_update'),
+    url(r'^layer_boundingbox_from_data/$', 'gvsigol_services.views.layer_boundingbox_from_data', name='layer_boundingbox_from_data')
 ]
