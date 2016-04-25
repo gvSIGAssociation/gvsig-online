c035b82d14702cc632cd7d31cb8db239ad0e3e7e
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2016-04-25 21:40:16 +0000

    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol_core@1103 216424d3-d1a8-4ca4-951f-a609719bba38

--- static/js/lib/viewer.js
+++ static/js/lib/viewer.js
@@ -29,14 +29,36 @@ viewer.core = {
 		
 	map: null,
 	
+	toolbar: null,
+	
+	tools: new Array(),
+	
+	legend: null,
+	
+	search: null,
+	
+	layerTree: null,
+	
 	layerCount: 0,
 		
     initialize: function(conf) {
     	this._createMap(conf);
-    	this._createToc(conf);
+    	this._initToolbar();
+    	this._loadLayers(conf);
+    	this._createWidgets(conf);    	    	
+    	this._loadTools(conf);
     },
     
     _createMap: function(conf) {
+    	
+    	var blank = new ol.layer.Tile({
+    		id: this._nextLayerId(),
+    		label: gettext('Blank'),
+          	visible: false,
+    	    source: new ol.source.XYZ({
+    	       url: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQIHWNgYGD4DwABBAEAHnOcQAAAAABJRU5ErkJggg=="
+    	    })
+    	});
     	
     	var osm = new ol.layer.Tile({
     		id: this._nextLayerId(),
@@ -57,35 +79,194 @@ viewer.core = {
       		],
       		renderer: 'canvas',
       		target: 'map',
-      		layers: [osm],
+      		layers: [blank, osm],
 			view: new ol.View({
-        		center: ol.proj.transform([40.39676430557205, -3.4716796875000004], 'EPSG:4326', 'EPSG:3857'),
+        		center: ol.proj.transform([parseFloat(conf.view.center_lon), parseFloat(conf.view.center_lat)], 'EPSG:4326', 'EPSG:3857'),
         		minZoom: 0,
         		maxZoom: 19,
-            	zoom: 6
+            	zoom: conf.view.zoom
         	})
 		});
     },
     
-    _createToc: function(conf) {   
-    	//jQuery UI sortable for the todo list
-    	$(".layer-tree").sortable({
-    		placeholder: "sort-highlight",
-    		handle: ".handle",
-    		forcePlaceholderSize: true,
-    		zIndex: 999999
+    _initToolbar: function() {
+    	var self = this;
+    	
+    	if (ol.has.TOUCH) {
+    		$(".toolbar-button").css("font-size", "1.5em");
+    	}
+    	
+    	$('#toolbar').on( "control-active", function(e) {
+    		  for (var i=0; i<self.tools.length; i++){
+    			  if (e.target.id != self.tools[i].id) {
+    				  if (self.tools[i].deactivable == true) {
+    					  if (self.tools[i].active) {
+        					  self.tools[i].deactivate();
+        				  }
+    				  }
+    			  }
+    		  }
+    		  if (self.layerTree.getEditionBar()) {
+    			  self.layerTree.getEditionBar().deactivateControls(); 
+    		  }   		  
     	});
-    	/* The todo list plugin */
-    	$(".layer-tree").layerTree({
-    		onCheck: function (ele) {
-    			window.console.log("The element has been checked");
-    			return ele;
-    		},
-    		onUncheck: function (ele) {
-    			window.console.log("The element has been unchecked");
-    			return ele;
+    },
+    
+    _loadLayers: function(conf) {
+    	this._loadBaseLayers(conf);
+    	this._loadOverlays(conf);
+    	//this._loadLayerGroups(conf);
+    },
+    
+    _createWidgets: function(conf) {   
+    	this.layerTree = new layerTree(conf, this.map);
+    	this.legend = new legend(conf, this.map);
+    	this.search = new search(conf, this.map);
+    },
+    
+    _loadBaseLayers: function(conf) {		
+		//BING LAYERS
+    	if (conf.base_layers.bing.active) {
+    		var bingTypes = [
+    		    'Road',
+    		    'Aerial',
+    		    'AerialWithLabels'
+    		];
+    		var i, ii;
+    		for (i = 0, ii = bingTypes.length; i < ii; ++i) {
+    			var bingLayer = new ol.layer.Tile({
+    				id: this._nextLayerId(),
+    				visible: false,
+    				label: bingTypes[i],
+    				preload: Infinity,
+    				source: new ol.source.BingMaps({
+    					key: conf.base_layers.bing.key,
+    					imagerySet: bingTypes[i]
+    				})
+    			});
+    			bingLayer.baselayer = true;
+    			this.map.addLayer(bingLayer);
     		}
-    	});
+    	}
+	},
+	
+	_loadOverlays: function(conf) {
+		var self = this;
+		for (var i=0; i<conf.layerGroups.length; i++) {			
+			var group = conf.layerGroups[i];
+			for (var k=0; k<group.layers.length; k++) {
+				var layerConf = group.layers[k];
+				var layerId = this._nextLayerId();
+				layerConf.id = layerId;
+				var url = layerConf.wms_url;
+				if (layerConf.cached) {
+					url = layerConf.cache_url;
+				}
+				
+				var wmsLayer = null;
+				if (layerConf.single_image) {
+					var wmsSource = new ol.source.ImageWMS({
+						url: url,
+						visible: layerConf.visible,
+						params: {'LAYERS': layerConf.workspace + ':' + layerConf.name, 'FORMAT': 'image/png', 'VERSION': '1.1.1'},
+						serverType: 'geoserver'
+					});
+					wmsLayer = new ol.layer.Image({
+						id: layerId,
+						source: wmsSource,
+						visible: layerConf.visible
+					});
+					
+				} else {
+					var wmsSource = new ol.source.TileWMS({
+						url: url,
+						visible: layerConf.visible,
+						params: {'LAYERS': layerConf.workspace + ':' + layerConf.name, 'FORMAT': 'image/png', 'VERSION': '1.1.1'},
+						serverType: 'geoserver'
+					});
+					wmsLayer = new ol.layer.Tile({
+						id: layerId,
+						source: wmsSource,
+						visible: layerConf.visible
+					});
+				}
+				
+				wmsLayer.on('change:visible', function(){
+					self.legend.reloadLegend();
+				});
+				wmsLayer.baselayer = false;
+				wmsLayer.layer_name = layerConf.name;
+				wmsLayer.wms_url = layerConf.wms_url;
+				wmsLayer.wfs_url = layerConf.wfs_url;
+				wmsLayer.cache_url = layerConf.cache_url;
+				wmsLayer.title = layerConf.title;
+				wmsLayer.legend = layerConf.legend;
+				wmsLayer.queryable = layerConf.queryable;
+				wmsLayer.is_vector = layerConf.is_vector;
+				wmsLayer.write_roles = layerConf.write_roles;
+				wmsLayer.namespace = layerConf.namespace;
+				wmsLayer.workspace = layerConf.workspace
+				wmsLayer.crs = layerConf.crs;
+				if (layerConf.is_time_layer) {
+					wmsLayer.isTimeLayer = true;
+					wmsLayer.timeParams = layerConf.time_params;
+				} else {
+					wmsLayer.isTimeLayer = false;
+				}
+				this.map.addLayer(wmsLayer);
+			}
+		}
+	},
+	
+	_loadLayerGroups: function(conf) {
+		var self = this;
+		for (var i=0; i<conf.layerGroups.length; i++) {			
+			var group = conf.layerGroups[i];
+			var url = null;
+			var cached = false;
+			//cached = group.cached;
+			
+			if (cached) {
+				url = conf.geoserver_base_url + '/gwc/service/wms';
+			} else {
+				url = conf.geoserver_base_url + '/wms';
+			}
+			
+			var layerGroupSource = new ol.source.TileWMS({
+				url: url,
+				params: {'LAYERS': group.groupName, 'FORMAT': 'image/png', 'VERSION': '1.1.1'},
+				//crossOrigin: '*',
+				serverType: 'geoserver'
+			});
+			var layerGroup = new ol.layer.Tile({			
+				id: group.groupName,
+				source: layerGroupSource,
+				visible: false
+			});
+				
+			layerGroup.on('change:visible', function(){
+				self.legend.reloadLegend();
+			});
+			layerGroup.baselayer = false;
+			layerGroup.layer_name = group.groupName;
+			layerGroup.wms_url = conf.geoserver_base_url + '/wms';
+			layerGroup.wfs_url = conf.geoserver_base_url + '/wfs';
+			layerGroup.title = group.groupTitle;
+			layerGroup.legend = conf.geoserver_base_url + '/wms' + '?SERVICE=WMS&VERSION=1.1.1&layer=' + group.groupName + '&REQUEST=getlegendgraphic&FORMAT=image/png';
+			layerGroup.queryable = true;
+			layerGroup.isTimeLayer = false;
+			layerGroup.isLayerGroup = true;
+			this.map.addLayer(layerGroup);
+		}
+	},
+	
+	_loadTools: function(conf) {
+    	this.tools.push(new getFeatureInfo(this.map, conf.tools.get_feature_info_control.private_fields_prefix));
+    	this.tools.push(new measureLength(this.map));
+    	this.tools.push(new measureArea(this.map));
+    	this.tools.push(new exportToPDF(this.map));
+    	this.tools.push(new geolocation(this.map));
+    	this.map.tools = this.tools;
     },
     
     _nextLayerId: function() {
