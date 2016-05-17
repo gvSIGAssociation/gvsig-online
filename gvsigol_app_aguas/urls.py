452545e53da345417fc9e3a3595e704502b4519d
jvhigon <jvhigon@216424d3-d1a8-4ca4-951f-a609719bba38>
2016-05-17 17:19:08 +0000

    quitando el código que irá en otro plugin
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol@1214 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol_app_aguas/urls.py
+++ gvsigol_app_aguas/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', 'gvsigol_app_aguas.views.index', name='index'), 
-    url(r'^create_project_wizard/$', 'gvsigol_app_aguas.views.create_project_wizard', name='create_project_wizard'),
 ]
