d0fe7971e281702055775c332a2c3ae5d95d847c
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2016-06-22 10:08:34 +0000

    git-svn-id: https://svn.scolab.es/gvsig-online/gvsigonline_v2/trunk@1294 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_filemanager/urls.py
+++ gvsigol/gvsigol_filemanager/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, patterns
 from django.views.decorators.csrf import csrf_exempt
 
-from views import BrowserView, DetailView, UploadView, UploadFileView, DirectoryCreateView
+from views import BrowserView, DetailView, UploadView, UploadFileView, DeleteFileView, DirectoryCreateView
 
 
 urlpatterns = patterns('',
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     url(r'^detail/$', DetailView.as_view(), name='detail'),
     url(r'^upload/$', UploadView.as_view(), name='upload'),
     url(r'^upload/file/$', csrf_exempt(UploadFileView.as_view()), name='upload-file'),
+    url(r'^delete/file/$', csrf_exempt(DeleteFileView.as_view()), name='delete-file'),
     url(r'^create/directory/$', DirectoryCreateView.as_view(), name='create-directory'),
 )
