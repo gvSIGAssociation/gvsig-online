a334612564af80ddb714d9e2510ee7f521366824
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2016-06-30 08:19:16 +0000

    git-svn-id: https://svn.scolab.es/gvsig-online/gvsigonline_v2/trunk@1369 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_filemanager/forms.py
+++ gvsigol/gvsigol_filemanager/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.utils.translation import ugettext as _
 
 class DirectoryCreateForm(forms.Form):
-    directory_name = forms.CharField( widget=forms.TextInput(attrs={'class' : 'form-control', 'tabindex': '1'}))
+    directory_name = forms.CharField(label=_(u'Directory name'), widget=forms.TextInput(attrs={'class' : 'form-control', 'tabindex': '1'}))
