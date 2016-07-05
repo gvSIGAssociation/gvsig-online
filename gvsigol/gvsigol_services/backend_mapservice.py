a568572ebad812fab5bff5d1f82011444396e5bc
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2016-07-05 08:14:17 +0000

    git-svn-id: https://svn.scolab.es/gvsig-online/gvsigonline_v2/trunk@1384 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_services/backend_mapservice.py
+++ gvsigol/gvsigol_services/backend_mapservice.py
@@ -343,7 +343,6 @@ class Geoserver():
         
         data += '<?xml version="1.0" encoding="ISO-8859-1"?>' 
         data += '<StyledLayerDescriptor version="1.0.0" xmlns="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc" '
-        data += 'xmlns:sld="http://www.opengis.net/sld"  xmlns:gml="http://www.opengis.net/gml" ' 
         data += 'xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '
         data += 'xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd">'
         data +=     '<NamedLayer>' 
