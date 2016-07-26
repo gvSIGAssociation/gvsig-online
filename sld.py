b72120f01fde725d99875f59c3c9b5b504da7741
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2016-07-26 17:02:23 +0000

    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol_symbology@1416 216424d3-d1a8-4ca4-951f-a609719bba38

--- sld.py
+++ sld.py
@@ -385,7 +385,8 @@ except ImportError as exp:
 # Globals
 #
 
-ExternalEncoding = 'ascii'
+#ExternalEncoding = 'ascii'
+ExternalEncoding = 'utf-8'
 Tag_pattern_ = re_.compile(r'({.*})?(.*)')
 String_cleanup_pat_ = re_.compile(r"[\n\r\s]+")
 Namespace_extract_pat_ = re_.compile(r'{(.*)}(.*)')
