fa3bd2b54829b56ea03df867e296c1772e9e0f5c
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2016-12-02 11:45:42 +0000

    Config gdaltools base path using settings.py variables
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol@1833 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol_core/apps.py
+++ gvsigol_core/apps.py
@@ -5,3 +5,16 @@ from django.apps import AppConfig
 
 class GvsigolCoreConfig(AppConfig):
     name = 'gvsigol_core'
+
+    def ready(self):
+        try:
+            # ensure we have a proper environment
+            self.config_gdaltools()
+        except:
+            pass
+
+    def config_gdaltools(self):
+        import gdaltools
+        from gvsigol.settings import GDALTOOLS_BASEPATH
+        if GDALTOOLS_BASEPATH != '':
+            gdaltools.Wrapper.BASEPATH = GDALTOOLS_BASEPATH
