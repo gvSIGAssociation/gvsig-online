614019ab537b35cc14d2426aa91fc3ab24470cf9
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2016-12-07 15:52:36 +0000

    Use commit method
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@1846 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_sync/gvsigol_plugin_sync/urls.py
+++ plugin_sync/gvsigol_plugin_sync/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^sync/layerinfo/([^/]+)/$', 'gvsigol_plugin_sync.views.get_layerinfo', name='get_layerinfo_by_project'),
     url(r'^sync/download/$', 'gvsigol_plugin_sync.views.sync_download', name='sync_download'),
     url(r'^sync/upload/$', 'gvsigol_plugin_sync.views.sync_upload', name='sync_upload'),
-    url(r'^sync/commit/$', 'gvsigol_plugin_sync.views.sync_upload', name='sync_commit'),
+    url(r'^sync/commit/$', 'gvsigol_plugin_sync.views.sync_commit', name='sync_commit'),
 ]
