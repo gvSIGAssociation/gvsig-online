72f624f45edcb6f568568dd0f17e0d05602d46dc
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2017-01-24 16:14:52 +0000

    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol@1972 216424d3-d1a8-4ca4-951f-a609719bba38

--- settings_template.py
+++ settings_template.py
@@ -193,6 +193,7 @@ SITE_ID=1
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+BASE_URL = '##BASE_URL##'
 MEDIA_ROOT = '/var/www/media/'
 MEDIA_URL = '##BASE_URL##/media/'
 STATIC_URL = '/static/'
