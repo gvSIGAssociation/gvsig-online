d1cdf84ee576d95ac0fab194c395cfb83397a235
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2017-02-06 15:20:04 +0000

    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/tags/gvsigol_v2.1.7@2031 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_filemanager/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-from django.utils.translation import ugettext as _
-
-class DirectoryCreateForm(forms.Form):
-    directory_name = forms.CharField(label=_(u'Directory name'), widget=forms.TextInput(attrs={'class' : 'form-control', 'tabindex': '1'}))
--- plugin_worldwind/gvsigol_plugin_worldwind/static/js/vendors/worldwind/README.txt
+++
@@ -1,5 +0,0 @@
-Some URLs has been modified:
-
-* OpenStreetMap: Mapquest has been replaced by OSM default (https://c.tile.openstreetmap.org/). Also credits.
-
-* Bing: Schema has been replaced by HTTPS
