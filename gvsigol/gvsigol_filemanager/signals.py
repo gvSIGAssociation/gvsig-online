8e70652528a61f9841c3685234c6b4ffcda2d17b
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2017-02-06 14:21:02 +0000

    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/tags/gvsigol_v2.1.7@2022 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_filemanager/signals.py
+++
@@ -1,4 +0,0 @@
-from django.dispatch import Signal
-
-filemanager_pre_upload = Signal(providing_args=["filename", "path", "filepath"])
-filemanager_post_upload = Signal(providing_args=["filename", "path", "filepath"])
--- plugin_catalog/gvsigol_plugin_catalog/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-class GvsigolCatalogConfig(AppConfig):
-    name = 'gvsigol_plugin_catalog'
-
-    def ready(self):
-        from gvsigol_plugin_catalog.service import geonetwork_service
