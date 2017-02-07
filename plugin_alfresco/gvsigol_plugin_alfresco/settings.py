35f56b8dca8c0a52d3d201bee15a78182bd35830
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2017-02-07 16:56:19 +0000

    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/tags/gvsigol_v2.1.7@2056 216424d3-d1a8-4ca4-951f-a609719bba38

---
+++ plugin_alfresco/gvsigol_plugin_alfresco/settings.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+ALFRESCO_REPOSITORY_URL = 'https://devel.gvsigonline.com/share/page/repository'
+ALFRESCO_CMIS_REST_API_URL = 'https://devel.gvsigonline.com:9443/alfresco/cmisatom'
+ALFRESCO_ADMIN_USER = 'admin'
+ALFRESCO_ADMIN_PASSWORD = 'GE2wa8RE'
