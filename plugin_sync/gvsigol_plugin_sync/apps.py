448ff6331195c9c9008c72f0840e9444899156d9
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2017-02-21 09:06:44 +0000

    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/tags/gvsigol_v2.1.7@2070 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_sync/gvsigol_plugin_sync/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class GvsigolSyncConfig(AppConfig):
-    name = 'gvsigol_app_sync'
--- plugin_worldwind/gvsigol_plugin_worldwind/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.utils.translation import ugettext as _
-from gvsigol_core.models import Project
-
-class DirectoryPath(forms.Form):
-    #name = forms.CharField(label=_(u'Provider name'), widget=forms.TextInput(attrs={ 'id': 'id_provider', 'class' : 'form-control', 'tabindex': '1'}))
-    projects = forms.ModelChoiceField(label=_(u'Project'), required=True, queryset=Project.objects.all(), widget=forms.Select(attrs={'class':'form-control'}))
-    directory_path = forms.CharField(label=_(u'Directory name'), widget=forms.TextInput(attrs={ 'id': 'id_directory', 'class' : 'form-control', 'tabindex': '1'}))
