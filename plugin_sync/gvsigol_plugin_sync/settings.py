8bcca01436d345a227eb928e8d57a00d3547dec6
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2017-02-23 11:09:51 +0000

    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/tags/gvsigol_v2.1.7@2085 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_sync/gvsigol_plugin_sync/settings.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-GVSIGOL_APP_DOWNLOAD_LINK = 'https://sigpobla.gvsigonline.com/media/data/documentacion/geopaparazzi-23-dic-2016.apk'
--- plugin_sync/gvsigol_plugin_sync/settings_template.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-GVSIGOL_APP_DOWNLOAD_LINK = '##GVSIGOL_APP_DOWNLOAD_LINK##'
