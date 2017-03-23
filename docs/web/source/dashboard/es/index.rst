4102e8560553de09a2be7e621ddc93a233debb00
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2017-03-23 12:38:07 +0000

    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/tags/gvsigol_v2.1.7@2222 216424d3-d1a8-4ca4-951f-a609719bba38

---
+++ docs/web/source/dashboard/es/index.rst
@@ -0,0 +1,11 @@
+Guía de administración
+======================
+
+.. toctree::
+   :maxdepth: 2
+   
+   users_and_groups
+   services
+   files
+   projects
+   symbology
---
+++ plugin_print/gvsigol_plugin_print/settings.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+PRINT_PROVIDER = {
+    'url': 'https://localhost/print',
+    'legal_warning': 'La información y cartografía disponible en este geoportal es propiedad de ........'
+}
