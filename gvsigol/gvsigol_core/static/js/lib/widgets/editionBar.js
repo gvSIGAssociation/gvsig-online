aa04e486fdcc9222fb4b42c428035964e668857a
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2017-04-11 14:38:23 +0000

    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@2299 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_core/static/js/lib/widgets/editionBar.js
+++ gvsigol/gvsigol_core/static/js/lib/widgets/editionBar.js
@@ -30,6 +30,8 @@ var editionBar = function(layerTree, map, featureType, selectedLayer) {
 	this.map = map;
 	this.layerTree = layerTree;
 	this.selectedLayer = selectedLayer;
+	this.selectedFeatures = new ol.Collection();
+	this.removedFeatures = new ol.Collection();
 	this.resourceManager = null;
 	if (layerTree.conf.resource_manager == 'gvsigol') {
 		this.resourceManager = new GvsigolResourceManager(this.selectedLayer);
@@ -286,6 +288,16 @@ editionBar.prototype.selectInteraction = null;
  */
 editionBar.prototype.removeInteraction = null;
 
+/**
+ * TODO
+ */
+editionBar.prototype.selecDragBox = null;
+
+/**
+ * TODO
+ */
+editionBar.prototype.removeDragBox = null;
+
 
 
 /**
@@ -432,17 +444,40 @@ editionBar.prototype.addModifyInteraction = function() {
 		wrapX: false
 	});
 	
+    this.selecDragBox = new ol.interaction.DragBox({
+    	condition: ol.events.condition.mouseOnly,
+    	style: new ol.style.Style({
+    		stroke: new ol.style.Stroke({
+    			color: [0, 0, 255, 1]
+    		})
+    	})
+    });
+	
 	this.modifyInteraction = new ol.interaction.Modify({
-		features: this.selectInteraction.getFeatures()
+		features: this.getFeaturesForModify()
 	});
 	
 	this.map.addInteraction(this.selectInteraction);
 	this.map.addInteraction(this.modifyInteraction);
+	this.map.addInteraction(this.selecDragBox);
 	
-	this.selectInteraction.on('select',
+	this.selecDragBox.on('boxend', function(evt) {
+        var info = [];
+        var extent = self.selecDragBox.getGeometry().getExtent();
+        self.source.forEachFeatureIntersectingExtent(extent, function(feature) {
+          self.selectedFeatures.push(feature);
+        });
+        self.editFeatureForm(self.selectedFeatures.getArray()[0]);
+      });
+	
+	this.selecDragBox.on('boxstart', function() {
+        self.selectedFeatures.clear();
+      });
+	
+	/*this.selectInteraction.on('select',
 		function(evt) {
 			self.editFeatureForm(evt.selected[0]);
-		}, this);
+		}, this);*/
 	
 	this.modifyInteraction.on('modifystart',
 		function(evt) {
@@ -453,6 +488,15 @@ editionBar.prototype.addModifyInteraction = function() {
 		function(evt) {
 			self.editFeatureForm(evt.features.getArray()[0]);
 		}, this);
+};
+
+/**
+ * TODO
+ */
+editionBar.prototype.getFeaturesForModify = function() {
+	return this.selectedFeatures;
+	
+	
 };
 
 
@@ -467,12 +511,30 @@ editionBar.prototype.addRemoveInteraction = function() {
 		wrapX: false
 	});
 	
-	this.map.addInteraction(this.removeInteraction);
+	this.removeDragBox = new ol.interaction.DragBox({
+    	condition: ol.events.condition.mouseOnly
+    });
 	
-	this.removeInteraction.on('select',
+	this.map.addInteraction(this.removeInteraction);
+	this.map.addInteraction(this.removeDragBox);
+	
+	/*this.removeInteraction.on('select',
 	    function(evt) {
 			self.removeFeatureForm(evt, evt.selected[0]);
-	   	}, this);
+	   	}, this);*/
+	
+	this.removeDragBox.on('boxend', function(evt) {
+        var info = [];
+        var extent = self.removeDragBox.getGeometry().getExtent();
+        self.source.forEachFeatureIntersectingExtent(extent, function(feature) {
+          self.removedFeatures.push(feature);
+        });
+        self.removeFeatureForm(self.removedFeatures.getArray()[0]);
+      });
+	
+	this.removeDragBox.on('boxstart', function() {
+        self.removedFeatures.clear();
+      });
 };
 
 
@@ -519,6 +581,18 @@ editionBar.prototype.deactivateControls = function() {
 	if (this.removeInteraction != null) {
 		this.map.removeInteraction(this.removeInteraction);
 		this.removeInteraction = null;
+	}
+	
+	if (this.selecDragBox != null) {
+		this.map.removeInteraction(this.selecDragBox);
+		this.selecDragBox = null;
+		this.selectedFeatures.clear();
+	}
+	
+	if (this.removeDragBox != null) {
+		this.map.removeInteraction(this.removeDragBox);
+		this.removeDragBox = null;
+		this.removedFeatures.clear();
 	}
 
 };
@@ -817,7 +891,7 @@ editionBar.prototype.editFeatureForm = function(feature) {
 /**
  * @param {Event} e Browser event.
  */
-editionBar.prototype.removeFeatureForm = function(evt, feature) {	
+editionBar.prototype.removeFeatureForm = function(feature) {	
 	
 	this.showDetailsTab();
 	
