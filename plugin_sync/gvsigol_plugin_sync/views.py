1a085a60dc7316ea5b2fb2681eb882868f9caeb0
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2017-04-17 07:49:42 +0000

    git-svn-id: https://svn.scolab.es/gvsig-online/gvsigonline_v2/trunk@2310 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_sync/gvsigol_plugin_sync/views.py
+++ plugin_sync/gvsigol_plugin_sync/views.py
@@ -1,6 +1,6 @@
 '''
     gvSIG Online.
-    Copyright (C) 2016 gvSIG Association.
+    Copyright (C) 2010-2017 SCOLAB.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
