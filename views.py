50056bc0a6003c1caf5c79fb30a96ce45d759de4
jbadia <jbadia@216424d3-d1a8-4ca4-951f-a609719bba38>
2017-05-19 06:39:59 +0000

    cron
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol_services@2525 216424d3-d1a8-4ca4-951f-a609719bba38

--- views.py
+++ views.py
@@ -1147,8 +1147,8 @@ def get_feature_info(request):
         lang = request.LANGUAGE_CODE
         if 'username' in request.session and 'password' in request.session:
             if request.session['username'] is not None and request.session['password'] is not None:
-                #req.auth = (request.session['username'], request.session['password'])
-                req.auth = ('admin', 'geoserver')
+                req.auth = (request.session['username'], request.session['password'])
+                #req.auth = ('admin', 'geoserver')
                 
         features = None           
         try:
