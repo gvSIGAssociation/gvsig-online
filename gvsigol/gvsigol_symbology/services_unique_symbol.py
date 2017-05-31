c69c9e8205a5414c1ae1d808a587dc5f4ff6ecb1
jbadia <jbadia@216424d3-d1a8-4ca4-951f-a609719bba38>
2017-05-31 14:14:12 +0000

    refs #2039
    git-svn-id: https://svn.scolab.es/gvsig-online/gvsigonline_v2/trunk@2584 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_symbology/services_unique_symbol.py
+++ gvsigol/gvsigol_symbology/services_unique_symbol.py
@@ -326,7 +326,7 @@ def get_conf(request, layer_id):
         'layer_name': workspace.name + ':' + layer.name,
         'style_name': workspace.name + '_' + layer.name + '_' + str(index),
         'libraries': Library.objects.all(),
-        'supported_crs': core_utils.get_supported_crs(),
+        'supported_crs': json.dumps(core_utils.get_supported_crs()),
         'preview_url': preview_url
     }    
      
