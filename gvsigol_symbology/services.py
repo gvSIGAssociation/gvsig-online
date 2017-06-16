45b74625fde58edcf3cc95c575a873ab885dc190
jbadia <jbadia@216424d3-d1a8-4ca4-951f-a609719bba38>
2017-06-16 08:59:00 +0000

    refs #2124 traducciones
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol@2756 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol_symbology/services.py
+++ gvsigol_symbology/services.py
@@ -68,7 +68,7 @@ def create_default_style(layer_id, style_name, style_type, geom_type):
     rule = Rule(
         style = style,
         name = 'Default symbol',
-        title = 'Default symbol',
+        title = _('Default symbol'),
         abstract = '',
         filter = str(""),
         minscale = -1,
@@ -351,7 +351,7 @@ def clone_style(mapservice, layer, original_style_name, cloned_style_name):
         rule = Rule(
             style = style,
             name = 'Default symbol',
-            title = 'Default symbol',
+            title = _('Default symbol'),
             abstract = '',
             filter = str(""),
             minscale = -1,
