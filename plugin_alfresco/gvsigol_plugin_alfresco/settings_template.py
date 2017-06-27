01e4cf03a515c91b66c3429a0c69943ffbae4653
jvhigon <jvhigon@216424d3-d1a8-4ca4-951f-a609719bba38>
2017-06-27 18:26:13 +0000

    corrigiendo template de configuración de Alfresco
    git-svn-id: https://svn.scolab.es/gvsig-online/gvsigonline_v2/trunk@2833 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_alfresco/gvsigol_plugin_alfresco/settings_template.py
+++ plugin_alfresco/gvsigol_plugin_alfresco/settings_template.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-ALFRESCO_REPOSITORY_URL = 'https://devel.gvsigonline.com/share/page/repository'
-ALFRESCO_CMIS_REST_API_URL = 'https://devel.gvsigonline.com/alfresco/cmisatom'
-ALFRESCO_ADMIN_USER = 'Admin'
-ALFRESCO_ADMIN_PASSWORD = 'admin'
+ALFRESCO_REPOSITORY_URL = 'https://##OS_HOST##/share/page/repository'
+ALFRESCO_CMIS_REST_API_URL = 'https://##OS_HOST##/alfresco/cmisatom'
+ALFRESCO_ADMIN_USER = 'root'
+ALFRESCO_ADMIN_PASSWORD = '##GEOSERVER_ADMIN_PASS##'
