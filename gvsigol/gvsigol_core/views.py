d1e3b15660cdc54f44f16d08f8ce5a5ea1ef89cd
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2017-07-26 09:10:53 +0000

    git-svn-id: https://svn.scolab.es/gvsig-online/gvsigonline_v2/trunk@3004 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_core/views.py
+++ gvsigol/gvsigol_core/views.py
@@ -556,6 +556,7 @@ def project_get_conf(request):
                 read_roles = services_utils.get_read_roles(l)
                 write_roles = services_utils.get_write_roles(l)
                 
+                '''
                 readable = False
                 if len(read_roles) == 0:
                     readable = True
@@ -564,7 +565,9 @@ def project_get_conf(request):
                         for rr in read_roles:
                             if ur == rr:
                                 readable = True
+                '''
                 
+                readable = True
                 if readable:             
                     layer = {}                
                     layer['name'] = l.name
