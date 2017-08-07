43ddbf8454acdfad4cf5b7236179c5a54f93758a
jbadia <jbadia@216424d3-d1a8-4ca4-951f-a609719bba38>
2017-08-07 09:08:14 +0000

    refs #2337 traducciones
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol@3127 216424d3-d1a8-4ca4-951f-a609719bba38

--- urls.py
+++ urls.py
@@ -21,7 +21,7 @@ import settings
 handler404 = 'gvsigol_core.views.not_found_view'
 
 js_info_dict = {
-    'packages': ('gvsigol_auth','gvsigol_core','gvsigol_services','gvsigol_symbology','gvsigol_plugin_print'),
+    'packages': ('gvsigol_auth','gvsigol_core','gvsigol_services','gvsigol_symbology','gvsigol_plugin_print','gvsigol_plugin_etl'),
 }
 
 urlpatterns = [
