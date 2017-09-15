751f0cee2bf2c0385bbc19144e0c042e57724d1b
jvhigon <jvhigon@216424d3-d1a8-4ca4-951f-a609719bba38>
2017-09-15 07:25:58 +0000

    actualizando versión en el settings_template
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/tags/gvsigol_v2.2.3@3268 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol/settings_template.py
+++ gvsigol/gvsigol/settings_template.py
@@ -227,7 +227,7 @@ STATICFILES_FINDERS = (
     #'compressor.finders.CompressorFinder',
 )
 
-GVSIGOL_VERSION = '2.2.2'
+GVSIGOL_VERSION = '2.2.3'
 
 GVSIGOL_USERS_CARTODB = {
     'dbhost': '##DATABASE_HOST##',
