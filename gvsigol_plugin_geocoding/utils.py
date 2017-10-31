56ceb49ab7ac23ee85c7bad68ff11c336d10df51
jbadia <jbadia@216424d3-d1a8-4ca4-951f-a609719bba38>
2017-10-31 12:42:00 +0000

    refs #2451
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/plugin_geocoding@3523 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol_plugin_geocoding/utils.py
+++ gvsigol_plugin_geocoding/utils.py
@@ -71,7 +71,7 @@ http://localhost:8983/solr/<core_name>/update?stream.body=<delete><query>table_n
 def remove_solr_data(provider):
     #resource = provider.type+"-"+str(provider.pk)
     resource = str(provider.pk)
-    url = geocoding_settings.URL_SOLR+'/'+geocoding_settings.SOLR_CORE_NAME+"/update?stream.body=<delete><query>resource:"+resource+"</query></delete>&commit=true"
+    url = geocoding_settings.URL_SOLR+'/'+geocoding_settings.SOLR_CORE_NAME+"/update?stream.body=<delete><query>resource:*"+resource+"</query></delete>&commit=true"
     req = urllib2.urlopen(url)
     if req.code != 200:
         return False
