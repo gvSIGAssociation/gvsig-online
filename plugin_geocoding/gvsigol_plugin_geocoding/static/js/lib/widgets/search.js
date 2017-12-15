717e5705babb0465facb42b233d5c4ab97a8c0d0
jbadia <jbadia@216424d3-d1a8-4ca4-951f-a609719bba38>
2017-12-15 08:34:13 +0000

    refs #2570
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/tags/gvsigol_v2.2.3@3830 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_geocoding/gvsigol_plugin_geocoding/static/js/lib/widgets/search.js
+++ plugin_geocoding/gvsigol_plugin_geocoding/static/js/lib/widgets/search.js
@@ -212,6 +212,9 @@ search.prototype.initUI = function() {
  */
 search.prototype.locate = function(address, origin_srs, fromCombo) {	
 	var self = this;	
+	this.map.removeOverlay(this.popup);
+	this.popup = new ol.Overlay.Popup();
+	this.map.addOverlay(this.popup);
 	if(address != null){
 		var coordinate = ol.proj.transform([parseFloat(address.lng), parseFloat(address.lat)], origin_srs, 'EPSG:3857');	
 		if(fromCombo){
