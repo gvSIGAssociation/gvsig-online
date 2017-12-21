3f3854ce0f66c6b906fe2850cecf1aeea041838f
jbadia <jbadia@216424d3-d1a8-4ca4-951f-a609719bba38>
2017-12-21 08:11:49 +0000

    refs #2090
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol_symbology@3864 216424d3-d1a8-4ca4-951f-a609719bba38

--- static/js/lib/symbology/UniqueValues.js
+++ static/js/lib/symbology/UniqueValues.js
@@ -97,6 +97,7 @@ UniqueValues.prototype.applyRampColor = function(json_data) {
 			symbolizer["fill_opacity"] = colr_aux["alpha"];
 			symbolizer["stroke"] = colr_aux["color"];
 			symbolizer["stroke_opacity"] = colr_aux["alpha"];
+			symbolizer.updatePreview();
 		}
 		rule.preview();
 	}
