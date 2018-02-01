b020c4878c230f3d702de4321f9111ed3af134db
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2018-02-01 14:55:46 +0000

    refs # 2721 #2575
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@4125 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_core/static/js/lib/widgets/messageBox.js
+++ gvsigol/gvsigol_core/static/js/lib/widgets/messageBox.js
@@ -76,3 +76,58 @@ messageBox.prototype.show = function(type, msg) {
 	$('body').append(ui);
 	$('#modal-error').modal('show');
 };
+
+/**
+ * TODO.
+ */
+messageBox.prototype.showMultiLine = function(type, messages) {
+	
+	var title = '';
+	var style = '';
+	var icon = '';
+	
+	if (type == 'info') {
+		title = 'Info';
+		style = 'alert-info';
+		icon = 'fa-info';
+		
+	} else if (type == 'success') {
+		title = gettext('Success');
+		style = 'alert-success';
+		icon = 'fa-check';
+		
+	} else if (type == 'warning') {
+		title = gettext('Warning');
+		style = 'alert-warning';
+		icon = 'fa-warning';
+		
+	} else if (type == 'error') {
+		title = 'Error';
+		style = 'alert-danger';
+		icon = 'fa-ban';
+	}
+	
+	// remove any previous content
+	$('#modal-error').remove();
+	
+	var ui = '';
+	ui += '<div class="modal fade" id="modal-error" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">';
+	ui += 	'<div class="modal-dialog" role="document">';
+	ui += 		'<div class="modal-content">';
+	ui += 			'<div class="modal-body" style="padding: 0px !important;">';
+	ui += 				'<div class="alert ' + style + ' alert-dismissible" style="border-radius: 0px !important;">';
+	ui += 					'<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>';
+	ui += 					'<h4><i class="icon fa ' + icon + '"></i> ' + title + '</h4>';
+	ui +=					'<ul>'
+	for (var i=0; i<messages.length; i++) {
+		ui +=					'<li style="list-style-type: none;">' + messages[i] + '</li>';
+	}
+	ui +=					'</ul>'
+	ui += 				'</div>';
+	ui += 			'</div>';
+	ui += 		'</div>';
+	ui += 	'</div>';
+	ui += '</div>';
+	$('body').append(ui);
+	$('#modal-error').modal('show');
+};
