33b42155f1a0b0caf041dc982008f651f32bf7fa
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2018-02-21 14:33:37 +0000

    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@4252 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol/settings_test.py
+++ gvsigol/gvsigol/settings_test.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.gis',
+    #'django.contrib.gis',
     'django_jenkins',
 ]
 
