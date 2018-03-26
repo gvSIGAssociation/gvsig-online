04594c77b584f5e596a57ded01ac40ac4ba336bf
jbadia <jbadia@216424d3-d1a8-4ca4-951f-a609719bba38>
2018-03-26 11:38:56 +0000

    refs #2813
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/tags/gvsigol_v2.2.3@4427 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_core/static/js/lib/widgets/editionBar.js
+++ gvsigol/gvsigol_core/static/js/lib/widgets/editionBar.js
@@ -1204,8 +1204,13 @@ editionBar.prototype.createFeatureForm = function(feature) {
 		
 		$('#edit_feature_properties .form-control').on('blur', function (evt) {
 			var props = feature.getProperties();
-			props[evt.currentTarget.id] = evt.currentTarget.value;
-			feature.setProperties(props);
+			if(evt.currentTarget.type == "number" && evt.currentTarget.value == ""){
+				delete props[evt.currentTarget.id];
+				feature.setProperties(props);
+			}else{
+				props[evt.currentTarget.id] = evt.currentTarget.value;
+				feature.setProperties(props);
+			}
 		});
 		$('#edit_feature_properties .checkbox').on('blur', function (evt) {
 			var props = feature.getProperties();
