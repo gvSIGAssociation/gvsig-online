b6ce7818955ea09d9ae4dcb90d19c123c0868551
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2018-05-23 10:41:16 +0000

    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@4557 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_print/gvsigol_plugin_print/static/js/lib/tools/print.js
+++ plugin_print/gvsigol_plugin_print/static/js/lib/tools/print.js
@@ -199,8 +199,8 @@ print.prototype.createPrintJob = function(template) {
 		if (!mapLayers[i].baselayer && mapLayers[i].layer_name != 'plg_catastro' && !(mapLayers[i] instanceof ol.layer.Vector)) {
 			if (mapLayers[i].getVisible()) {
 				var layer = {
-					"baseURL": "http://localhost/gs-local/ws_jrodrigo/wms",
-					//"baseURL": mapLayers[i].wms_url_no_auth,
+					//"baseURL": "http://localhost/gs-local/ws_jrodrigo/wms",
+					"baseURL": mapLayers[i].wms_url_no_auth,
 			  	    "opacity": mapLayers[i].getOpacity(),
 			  	    "type": "WMS",
 		  			"imageFormat": "image/png",
@@ -222,14 +222,14 @@ print.prototype.createPrintJob = function(template) {
 				}
 				printLayers.push(layer);
 				
-				/*var legend = {
-					"name": mapLayers[i].title,
-		            "icons": [mapLayers[i].legend_no_auth]
-		        };*/
 				var legend = {
 					"name": mapLayers[i].title,
+		            "icons": [mapLayers[i].legend_no_auth]
+		        };
+				/*var legend = {
+					"name": mapLayers[i].title,
 			        "icons": ["http://localhost:8080/geoserver/ws_jrodrigo/wms?SERVICE=WMS&VERSION=1.1.1&layer=t$recinto&REQUEST=getlegendgraphic&FORMAT=image/png"]
-			    };
+			    };*/
 				legends.push(legend);
 			}									
 		}
@@ -335,8 +335,8 @@ print.prototype.createPrintJob = function(template) {
 		  			"scale": self.getCurrentScale(parseInt(dpi)),
 		  			"layers": printLayers
 		  	    },
-		  	    //"logo_url": self.conf.project_image,
-		  	    "logo_url": "https://demo.gvsigonline.com/media/images/igvsb.jpg",
+		  	    "logo_url": self.conf.project_image,
+		  	    //"logo_url": "https://demo.gvsigonline.com/media/images/igvsb.jpg",
 		  	    "legend": {
 		  	    	"name": "",
 		            "classes": legends
