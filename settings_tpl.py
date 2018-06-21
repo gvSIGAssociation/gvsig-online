c7a7244b1b1608626d3cf8f4a7cdb6b0ebf68f43
jvhigon <jvhigon@216424d3-d1a8-4ca4-951f-a609719bba38>
2018-06-21 12:05:20 +0000

    Nuevo mecanismo de despliegue adaptado a DGTIC refs #2915
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol@4693 216424d3-d1a8-4ca4-951f-a609719bba38

--- settings_tpl.py
+++ settings_tpl.py
@@ -245,7 +245,7 @@ SITE_ID=1
 BASE_URL = '##BASE_URL##'
 MEDIA_ROOT = '##MEDIA_ROOT##'
 MEDIA_URL = '##BASE_URL##/##MEDIA_PATH##/'
-STATIC_URL = '/##STATIC_PATH##'
+STATIC_URL = '/##STATIC_PATH##/'
 STATIC_ROOT = '##GVSIGOL_HOME##/gvsigol/assets'
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
