af161138cff19d27a387e3f077b6d0bc10f6ced9
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2018-09-11 14:58:32 +0000

    Geonetwork api version variable
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@4734 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_catalog/gvsigol_plugin_catalog/configure.sh
+++ plugin_catalog/gvsigol_plugin_catalog/configure.sh
@@ -2,6 +2,7 @@
 
 echo "Running install script for catalog plugin ..."
 mv settings_tpl.py settings.py
+grep -rl "##GEONETWORK_BASE_URL##" | xargs sed -i "s ##GEONETWORK_BASE_URL## $GEONETWORK_BASE_URL g" 
 grep -rl "##GEONETWORK_API_URL##" | xargs sed -i "s ##GEONETWORK_API_URL## $GEONETWORK_API_URL g" 
 grep -rl "##GEONETWORK_USER##" | xargs sed -i "s/##GEONETWORK_USER##/$GEONETWORK_USER/g"
 grep -rl "##GEONETWORK_PASS##" | xargs sed -i "s/##GEONETWORK_PASS##/$GVSIG_ONLINE_PASSWORD/g"
--- plugin_catalog/gvsigol_plugin_catalog/settings.py
+++ plugin_catalog/gvsigol_plugin_catalog/settings.py
@@ -2,3 +2,5 @@
 CATALOG_URL = 'http://localhost:8080/geonetwork/srv/spa/'
 CATALOG_USER = 'admin'
 CATALOG_PASSWORD = 'admin'
+# valid values: 'legacy3.2', 'api0.1'
+CATALOG_API_VERSION = 'api0.1'
