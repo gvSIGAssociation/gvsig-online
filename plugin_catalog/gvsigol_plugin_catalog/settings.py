7c9ed8984b3989b6309b933f9cdad37934e15c82
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2018-09-14 18:45:46 +0000

    Usa urls relativas a la base de geonetwork
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@4739 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_catalog/gvsigol_plugin_catalog/settings.py
+++ plugin_catalog/gvsigol_plugin_catalog/settings.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-CATALOG_URL = 'http://localhost:8080/geonetwork/srv/api/'
+CATALOG_BASE_URL = 'http://localhost:8080/geonetwork'
+CATALOG_URL = CATALOG_BASEURL + '/srv/eng/'
 CATALOG_USER = 'admin'
 CATALOG_PASSWORD = 'admin'
 # valid values: 'legacy3.2', 'api0.1'
