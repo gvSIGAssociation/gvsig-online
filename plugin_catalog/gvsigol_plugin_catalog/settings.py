eeaac7f8bcb80b631cadac75f65ab2f9279cbfaf
jbadia <jbadia@216424d3-d1a8-4ca4-951f-a609719bba38>
2018-09-17 08:59:19 +0000

    refs #2981 plugin-catalog - adapt to 3.4.x
    git-svn-id: https://svn.scolab.es/gvsig-online/gvsigonline_v2/trunk@4740 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_catalog/gvsigol_plugin_catalog/settings.py
+++ plugin_catalog/gvsigol_plugin_catalog/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 CATALOG_BASE_URL = 'http://localhost:8080/geonetwork'
-CATALOG_URL = CATALOG_BASEURL + '/srv/eng/'
+CATALOG_URL = CATALOG_BASE_URL + '/srv/eng/'
 CATALOG_USER = 'admin'
 CATALOG_PASSWORD = 'admin'
 # valid values: 'legacy3.2', 'api0.1'
