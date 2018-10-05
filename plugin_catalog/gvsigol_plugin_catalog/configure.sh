41c3ca0936161708f7ca72e76f78017875922f26
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2018-10-05 10:39:30 +0000

    Fix variable names
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@4757 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_catalog/gvsigol_plugin_catalog/configure.sh
+++ plugin_catalog/gvsigol_plugin_catalog/configure.sh
@@ -3,32 +3,42 @@
 echo "Running install script for catalog plugin ..."
 mv settings_tpl.py settings.py
 
-if [ -z $GEONETWORK_HOST ]; then
-        echo "WARNING: GEONETWORK_HOST is not defined, building using GVSIGOL_HOST ."
-        GEONETWORK_HOST=$FRONTEND_URL
+if [ -z "$GEONETWORK_HOST" ]; then
+        if [ -n "$FRONTEND_URL" ]; then
+                echo "WARNING: GEONETWORK_HOST is not defined, building using FRONTEND_URL."
+                GEONETWORK_HOST=$FRONTEND_URL
+        else
+                echo "WARNING: GEONETWORK_HOST is not defined, building using GVSIGOL_HOST."
+                GEONETWORK_HOST=$GVSIGOL_HOST
 fi
 grep -rl "##GEONETWORK_HOST##"  | xargs sed -i "s/##GEONETWORK_HOST##/$GEONETWORK_HOST/g"
 
-if [ -z $GEONETWORK_BASE_URL ]; then
-        echo "WARNING: GEONETWORK_BASE_URL is not defined, using HTTP_PROTOCOL and GEONETWORK_HOST ."
-        GEONETWORK_BASE_URL="$HTTP_PROTOCOL://$GEONETWORK_HOST/geonetwork"
+if [ -z "$CATALOG_BASE_URL" ]; then
+        echo "WARNING: CATALOG_BASE_URL is not defined, using HTTP_PROTOCOL and GEONETWORK_HOST ."
+        CATALOG_BASE_URL="$HTTP_PROTOCOL://$GEONETWORK_HOST/geonetwork"
 fi
 
-if [ -z $GEONETWORK_URL ]; then
-        echo "WARNING: GEONETWORK_URL is not defined, using HTTP_PROTOCOL and GEONETWORK_HOST ."
-        GEONETWORK_URL="$HTTP_PROTOCOL://$GEONETWORK_HOST/geonetwork/srv/eng/"
+if [ -z "$CATALOG_URL" ]; then
+        echo "WARNING: CATALOG_URL is not defined, using HTTP_PROTOCOL and GEONETWORK_HOST ."
+        CATALOG_URL="$HTTP_PROTOCOL://$GEONETWORK_HOST/geonetwork/srv/eng/"
 fi
 
-if [ -z $CATALOG_API_VERSION ]; then
+if [ -z "$CATALOG_API_VERSION" ]; then
         echo "WARNING: CATALOG_API_VERSION is not defined, using 'legacy3.2'."
         CATALOG_API_VERSION="legacy3.2"
 fi
+
+if [ -z "$GEONETWORK_USER" ]; then
+        echo "WARNING: GEONETWORK_USER is not defined, using 'admin'."
+        GEONETWORK_USER="admin"
+fi
+if [ -z "$GEONETWORK_PASS" ]; then
+        echo "WARNING: GEONETWORK_PASS is not defined, using 'admin'."
+        GEONETWORK_PASS="admin"
+fi
 grep -rl "##CATALOG_API_VERSION##"  | xargs sed -i "s ##CATALOG_API_VERSION## $CATALOG_API_VERSION g"
-
-
-grep -rl "##GEONETWORK_URL##"  | xargs sed -i "s ##GEONETWORK_URL## $GEONETWORK_URL g"
-grep -rl "##GEONETWORK_BASE_URL##" | xargs sed -i "s ##GEONETWORK_BASE_URL## $GEONETWORK_BASE_URL g" 
-grep -rl "##GEONETWORK_API_VERSION##" | xargs sed -i "s ##GEONETWORK_API_VERSION## $GEONETWORK_API_VERSION g" 
+grep -rl "##CATALOG_URL##"  | xargs sed -i "s ##CATALOG_URL## $CATALOG_URL g"
+grep -rl "##CATALOG_BASE_URL##" | xargs sed -i "s ##CATALOG_BASE_URL## $CATALOG_BASE_URL g" 
 grep -rl "##GEONETWORK_USER##" | xargs sed -i "s/##GEONETWORK_USER##/$GEONETWORK_USER/g"
 grep -rl "##GEONETWORK_PASS##" | xargs sed -i "s/##GEONETWORK_PASS##/$GVSIG_ONLINE_PASSWORD/g"
 
