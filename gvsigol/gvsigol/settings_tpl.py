f2fef8894fd018f982497d37aad2ad358101a075
jvhigon <jvhigon@216424d3-d1a8-4ca4-951f-a609719bba38>
2018-10-17 09:52:54 +0000

    Migración carto.bombersdv.es refs  #2991 supported_crs
    git-svn-id: https://svn.scolab.es/gvsig-online/gvsigonline_v2/trunk@4798 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol/settings_tpl.py
+++ gvsigol/gvsigol/settings_tpl.py
@@ -313,15 +313,9 @@ SUPPORTED_CRS = {
         'definition': '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs',
         'units': 'meters'
     },
-    '900913': {
-        'code': 'EPSG:900913',
-        'title': 'Google Maps Global Mercator -- Spherical Mercator',
-        'definition': '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs',
-        'units': 'meters'
-    },
     '4326': {
         'code': 'EPSG:4326',
-        'title': 'WGS84',
+        'title': 'WGS84 / LatLon',
         'definition': '+proj=longlat +datum=WGS84 +no_defs +axis=neu',
         'units': 'degrees'
     },
