1c26ef18233241c99b3b0851051bec9f8dd4706f
jvhigon <jvhigon@216424d3-d1a8-4ca4-951f-a609719bba38>
2018-10-17 06:37:35 +0000

    Migración carto.bombersdv.es refs  #2991 colocando una url de catastro por defecto
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/plugin_catastro@4786 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol_plugin_catastro/configure.sh
+++ gvsigol_plugin_catastro/configure.sh
@@ -3,5 +3,11 @@
 
 echo "Running install script for cadastral plugin ..."	
 mv settings_tpl.py settings.py
+
+if [ -z $URL_CATASTRO ]; then
+			URL_CATASTRO="https://ovc.catastro.meh.es/Cartografia/WMS/ServidorWMS.aspx"
+			echo "WARNING: URL_CATASTRO is not defined, using default value $URL_CATASTRO"					
+fi	
+
 grep -rl "##URL_CATASTRO##" | xargs sed -i "s ##URL_CATASTRO## $URL_CATASTRO g"
 
