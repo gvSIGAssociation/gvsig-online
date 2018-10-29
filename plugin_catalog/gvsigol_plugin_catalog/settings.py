2eeab512bc98ace1f63bc3dd9218975b3d4a0ddd
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2018-10-29 15:55:47 +0000

    Restore settings
    git-svn-id: https://svn.scolab.es/gvsig-online/gvsigonline_v2/trunk@4831 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_catalog/gvsigol_plugin_catalog/settings.py
+++ plugin_catalog/gvsigol_plugin_catalog/settings.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-CATALOG_BASE_URL = 'https://localhost/geonetwork'
+CATALOG_BASE_URL = 'http://localhost:8080/geonetwork'
 CATALOG_URL = CATALOG_BASE_URL + '/srv/eng/'
 CATALOG_USER = 'admin'
-CATALOG_PASSWORD = 'gvsigol'
+CATALOG_PASSWORD = 'admin'
 # valid values: 'legacy3.2', 'api0.1'
 CATALOG_API_VERSION = 'api0.1'
