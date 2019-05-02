248b40777fc368d9159dafb7299c68cbebf99639
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2019-05-02 11:11:12 +0000

    Nueva implementación del Singleton
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol_services@4979 216424d3-d1a8-4ca4-951f-a609719bba38

--- geographic_servers.py
+++ geographic_servers.py
@@ -26,58 +26,16 @@ logger = logging.getLogger("gvsigol")
 
 
 from models import Server, Node
-           
-class GeographicServers:
-    class __impl:
-        def get_servers(self):
-            return self.servers
-        
-        def add_server(self, sv_conf):
-            ''' TO CHANGE '''
-            master_node = None
-            slave_nodes = []
-            for n in Node.objects.filter(server=sv_conf):
-                if n.is_master:
-                    master_node = n.url
-                else:
-                    slave_nodes.append(n.url)
-                    
-            geoserver = backend_geoserver.Geoserver(sv_conf.id, sv_conf.default, sv_conf.name, sv_conf.user, sv_conf.password, master_node, slave_nodes)
-            self.servers.append(geoserver)
-        
-        def get_server_by_name(self, name):
-            for s in self.servers:
-                if s.name == name:
-                    return s
-        
-        def get_server_by_id(self, id):
-            for s in self.servers:
-                if s.id == int(id):
-                    return s
-        
-        def get_default_server(self):
-            for s in self.servers:
-                if s.default:
-                    return s
-                
-        def get_master_node(self, id):
-            for n in Node.objects.filter(server_id=int(id)):
-                if n.is_master:
-                    return n
 
-    # storage for the instance reference
+class Singleton:
     __instance = None
-
+    def __new__(cls, *args, **kwargs):
+        if not cls.__instance:
+            cls.__instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
+        return cls.__instance
+           
+class GeographicServers(Singleton):
     def __init__(self):
-        """ Create singleton instance """
-        # Check whether we already have an instance
-        if GeographicServers.__instance is None:
-            # Create and remember instance
-            GeographicServers.__instance = GeographicServers.__impl()
-
-        # Store instance reference as the only member in the handle
-        self.__dict__['_Singleton__instance'] = GeographicServers.__instance
-        
         self.servers = []
         for s in Server.objects.all():
             master_node = None
@@ -90,13 +48,41 @@ class GeographicServers:
                     
             gs = backend_geoserver.Geoserver(s.id, s.default, s.name, s.user, s.password, master_node, slave_nodes)
             self.servers.append(gs)
-
-    def __getattr__(self, attr):
-        """ Delegate access to implementation """
-        return getattr(self.__instance, attr)
-
-    def __setattr__(self, attr, value):
-        """ Delegate access to implementation """
-        return setattr(self.__instance, attr, value)
+        
+    def get_servers(self):
+            return self.servers
+        
+    def add_server(self, sv_conf):
+        ''' TO CHANGE '''
+        master_node = None
+        slave_nodes = []
+        for n in Node.objects.filter(server=sv_conf):
+            if n.is_master:
+                master_node = n.url
+            else:
+                slave_nodes.append(n.url)
+                
+        geoserver = backend_geoserver.Geoserver(sv_conf.id, sv_conf.default, sv_conf.name, sv_conf.user, sv_conf.password, master_node, slave_nodes)
+        self.servers.append(geoserver)
+    
+    def get_server_by_name(self, name):
+        for s in self.servers:
+            if s.name == name:
+                return s
+    
+    def get_server_by_id(self, id):
+        for s in self.servers:
+            if s.id == int(id):
+                return s
+    
+    def get_default_server(self):
+        for s in self.servers:
+            if s.default:
+                return s
+            
+    def get_master_node(self, id):
+        for n in Node.objects.filter(server_id=int(id)):
+            if n.is_master:
+                return n      
 
 geographic_servers = GeographicServers()
