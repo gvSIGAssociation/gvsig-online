2c70af8d18243d718e3d1269f70a207f170cc606
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2019-05-06 17:23:04 +0000

    Usar variable de módulo para crear singleton
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@4991 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_services/geographic_servers.py
+++ gvsigol/gvsigol_services/geographic_servers.py
@@ -26,15 +26,8 @@ logger = logging.getLogger("gvsigol")
 
 
 from models import Server, Node
-
-class Singleton:
-    __instance = None
-    def __new__(cls, *args, **kwargs):
-        if not cls.__instance:
-            cls.__instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
-        return cls.__instance
            
-class GeographicServers(Singleton):
+class GeographicServers:
     def __init__(self):
         self.servers = []
         for s in Server.objects.all():
