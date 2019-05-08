5a489efd2486184435006bf34f00a3eea48b8aa9
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2019-05-08 14:39:46 +0000

    Adapt urls to Django 2.10+ syntax
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@5001 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_print/gvsigol_plugin_print/urls.py
+++ plugin_print/gvsigol_plugin_print/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
+from gvsigol_plugin_print import views as print_views
 
 urlpatterns = [
-    url(r'^print/get_conf/$', 'gvsigol_plugin_print.views.get_conf', name='get_conf'),
+    url(r'^print/get_conf/$', print_views.get_conf, name='get_conf'),
 ]
