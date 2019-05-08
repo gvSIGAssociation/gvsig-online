601d0675aa0b7c7bf850c3539ccda9f22ee6fafd
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2019-05-08 14:38:37 +0000

    Adapt urls to Django 2.10+ syntax
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol@5000 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/urls.py
+++ gvsigol/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url, patterns
+from django.conf.urls import include, url, i18n
 from django.views.i18n import javascript_catalog
 from django.contrib import admin
+
 import settings
 
 handler404 = 'gvsigol_core.views.not_found_view'
@@ -25,44 +26,44 @@ js_info_dict = {
 }
 
 urlpatterns = [
-    url(r'^gvsigonline/i18n/', include('django.conf.urls.i18n')),
+    url(r'^gvsigonline/i18n/', include(i18n)),
     url(r'^gvsigonline/jsi18n/$', javascript_catalog, js_info_dict),    
     url(r'^gvsigonline/admin/', include(admin.site.urls)),
 ]
 
 for app in settings.INSTALLED_APPS:
     if 'gvsigol_app_' in app:
-        urlpatterns += patterns('',
+        urlpatterns += [
             url(r'^gvsigonline/', include(app + '.urls')),
-        )
+        ]
         
 for plugin in settings.INSTALLED_APPS:
     if 'gvsigol_plugin_' in plugin:
-        urlpatterns += patterns('',
+        urlpatterns += [
             url(r'^gvsigonline/', include(plugin + '.urls')),
-        )
+        ]
     
 if 'gvsigol_core' in settings.INSTALLED_APPS:
-    urlpatterns += patterns('',
+    urlpatterns += [
         url(r'^gvsigonline/core/', include('gvsigol_core.urls')),
-    )
+    ]
   
 if 'gvsigol_auth' in settings.INSTALLED_APPS:
-    urlpatterns += patterns('',
+    urlpatterns += [
         url(r'^gvsigonline/auth/', include('gvsigol_auth.urls')),
-    )
+    ]
 
 if 'gvsigol_services' in settings.INSTALLED_APPS:
-    urlpatterns += patterns('',
+    urlpatterns += [
         url(r'^gvsigonline/services/', include('gvsigol_services.urls')),
-    )
+    ]
     
 if 'gvsigol_symbology' in settings.INSTALLED_APPS:
-    urlpatterns += patterns('',
+    urlpatterns += [
         url(r'^gvsigonline/symbology/', include('gvsigol_symbology.urls')),
-    )
+    ]
     
 if 'gvsigol_filemanager' in settings.INSTALLED_APPS:
-    urlpatterns += patterns('',
+    urlpatterns += [
         url(r'^gvsigonline/filemanager/', include('gvsigol_filemanager.urls', namespace='filemanager')),      
-    )
+    ]
--- gvsigol_filemanager/urls.py
+++ gvsigol_filemanager/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import url
 from django.views.decorators.csrf import csrf_exempt
 
 from views import BrowserView, ExportToDatabaseView, UploadView, UploadFileView, DeleteFileView, DirectoryCreateView
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', BrowserView.as_view(), name='browser'),
     #url(r'^pbrowser/$', PopupBrowserView.as_view(), name='pbrowser'),
     url(r'^upload/$', UploadView.as_view(), name='upload'),
@@ -12,4 +12,4 @@ urlpatterns = patterns('',
     url(r'^delete/file/$', csrf_exempt(DeleteFileView.as_view()), name='delete-file'),
     url(r'^create/directory/$', DirectoryCreateView.as_view(), name='create-directory'),
     url(r'^export_to_database/$', ExportToDatabaseView.as_view(), name='export-to-database'),
-)
+]
