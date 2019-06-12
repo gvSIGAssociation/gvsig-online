26faef506e7597b265b92fad0dfdf5e2cba79418
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2019-06-12 17:00:21 +0000

    refs #3451
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol@5085 216424d3-d1a8-4ca4-951f-a609719bba38

--- settings_tpl.py
+++ settings_tpl.py
@@ -365,7 +365,7 @@ CONTROL_FIELDS = [{
                 'type': 'date'
                 }]
 
-BASELAYER_SUPPORTED_TYPES = ['WMS', 'WMTS', 'XYZ', 'Bing', 'OSM']
+EXTERNAL_LAYER_SUPPORTED_TYPES = ['WMS', 'WMTS', 'XYZ', 'Bing', 'OSM']
 
 WMTS_MAX_VERSION = '1.0.0'
 WMS_MAX_VERSION = '1.3.0'
