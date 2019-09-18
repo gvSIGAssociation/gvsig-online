8d83496c01b4034940f281fd795a02fbad7dd1d4
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2019-09-18 12:05:04 +0000

    Adapt urls to django 1.11
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@5220 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_alfresco/gvsigol_plugin_alfresco/urls.py
+++ plugin_alfresco/gvsigol_plugin_alfresco/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
+from gvsigol_plugin_alfresco import views
 
 urlpatterns = [
-    url(r'^alfresco/get_sites/$', 'gvsigol_plugin_alfresco.views.get_sites', name='get_sites'),
-    url(r'^alfresco/get_folder_content/$', 'gvsigol_plugin_alfresco.views.get_folder_content', name='get_folder_content'),
-    url(r'^alfresco/save_resource/$', 'gvsigol_plugin_alfresco.views.save_resource', name='save_resource'),
-    url(r'^alfresco/update_resource/$', 'gvsigol_plugin_alfresco.views.update_resource', name='update_resource'),
-    url(r'^alfresco/delete_resource/$', 'gvsigol_plugin_alfresco.views.delete_resource', name='delete_resource'),
+    url(r'^alfresco/get_sites/$', views.get_sites, name='get_sites'),
+    url(r'^alfresco/get_folder_content/$', views.get_folder_content, name='get_folder_content'),
+    url(r'^alfresco/save_resource/$', views.save_resource, name='save_resource'),
+    url(r'^alfresco/update_resource/$', views.update_resource, name='update_resource'),
+    url(r'^alfresco/delete_resource/$', views.delete_resource, name='delete_resource'),
 ]
