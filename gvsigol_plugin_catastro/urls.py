17762b14996a1605750ba03db8d3b8797a28aed6
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2019-09-18 12:10:56 +0000

    Adapt urls to django 1.11
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/plugin_catastro@5223 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol_plugin_catastro/urls.py
+++ gvsigol_plugin_catastro/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import url
+from gvsigol_plugin_catastro import views
 
 urlpatterns = [
-    url(r'^catastro/get_conf/$', 'gvsigol_plugin_catastro.views.get_conf', name='get_conf'),
+    url(r'^catastro/get_conf/$', views.get_conf, name='get_conf'),
 
-    url(r'^catastro/get_provincias/$', 'gvsigol_plugin_catastro.views.get_provincias', name='get_provincias'),
-    url(r'^catastro/get_municipios/$', 'gvsigol_plugin_catastro.views.get_municipios', name='get_municipios'),
-    url(r'^catastro/get_vias/$', 'gvsigol_plugin_catastro.views.get_vias', name='get_vias'),
+    url(r'^catastro/get_provincias/$', views.get_provincias, name='get_provincias'),
+    url(r'^catastro/get_municipios/$', views.get_municipios, name='get_municipios'),
+    url(r'^catastro/get_vias/$', views.get_vias, name='get_vias'),
 
-    url(r'^catastro/get_referencia_catastral/$', 'gvsigol_plugin_catastro.views.get_referencia_catastral', name='get_referencia_catastral'),
-    url(r'^catastro/get_referencia_catastral_polygon/$', 'gvsigol_plugin_catastro.views.get_referencia_catastral_polygon', name='get_referencia_catastral_polygon'),
-    url(r'^catastro/get_rc_info/$', 'gvsigol_plugin_catastro.views.get_rc_info', name='get_rc_info'),
-    url(r'^catastro/get_rc_by_coords/$', 'gvsigol_plugin_catastro.views.get_rc_by_coords', name='get_rc_by_coords')
+    url(r'^catastro/get_referencia_catastral/$', views.get_referencia_catastral, name='get_referencia_catastral'),
+    url(r'^catastro/get_referencia_catastral_polygon/$', views.get_referencia_catastral_polygon, name='get_referencia_catastral_polygon'),
+    url(r'^catastro/get_rc_info/$', views.get_rc_info, name='get_rc_info'),
+    url(r'^catastro/get_rc_by_coords/$', views.get_rc_by_coords, name='get_rc_by_coords')
 ]
