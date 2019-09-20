5bcf260fdc74bfe296575488c11bcf558f74c780
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2019-09-20 21:44:18 +0000

    Add celary app
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@5242 216424d3-d1a8-4ca4-951f-a609719bba38

---
+++ gvsigol/gvsigol/celery.py
@@ -0,0 +1,28 @@
+"""
+ Defines the django instance to use
+ See https://docs.celeryproject.org/en/latest/django/first-steps-with-django.html#django-first-steps
+"""
+
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gvsigol.settings')
+
+app = Celery('gvsigol')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+#app.config_from_object('gvsigol.celeryconfig')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
