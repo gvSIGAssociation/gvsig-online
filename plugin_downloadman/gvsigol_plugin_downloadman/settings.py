ba095010d452c6088d2e17abbeb7b2c1debaf55c
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2019-10-04 09:22:46 +0000

    Update settings
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@5303 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_downloadman/gvsigol_plugin_downloadman/settings.py
+++ plugin_downloadman/gvsigol_plugin_downloadman/settings.py
@@ -2,5 +2,7 @@
 APP_TITLE = 'Gestor de descargas'
 APP_DESCRIPTION = 'Gestor de descargas'
 TMP_DIR="/tmp/gol-downman"
-#TARGET_DIR="/var/www/downloads"
-TARGET_DIR='/var/www/sites/gvsigol.localhost/media/downloads'
+TARGET_ROOT='/var/www/downloads'
+from gvsigol.settings import BASE_URL
+TARGET_URL = BASE_URL + '/media/downloads'
+LOCAL_PATHS_WHITELIST = ['/var/www/downloads']
