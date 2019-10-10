94a6c3e314e6f2c57ad82f6b394d29eca925e3f4
jvhigon <jvhigon@216424d3-d1a8-4ca4-951f-a609719bba38>
2019-10-10 12:08:40 +0000

    parametrizando GTFS_SCRIPT
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@5349 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_trip_planner/gvsigol_plugin_trip_planner/configure.sh
+++ plugin_trip_planner/gvsigol_plugin_trip_planner/configure.sh
@@ -3,7 +3,9 @@
 echo "Running install script for trip-planner ..."	
 mv settings_tpl.py settings.py
 
+if [ -z "$GTFS_SCRIPT" ]; then
+        echo "ERROR: GTFS_SCRIPT is not defined."
+        exit -1        
+fi
 
-
-
-
+grep -rl "##GTFS_SCRIPT##"  | xargs sed -i "s%%##GTFS_SCRIPT##%%$GTFS_SCRIPT%%g"
