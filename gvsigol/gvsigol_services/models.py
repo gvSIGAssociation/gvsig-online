46de4f70e5b8e50f18c018bc024fb4c2e56c7298
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2019-11-15 12:13:08 +0000

    refs #3774 refs #3775
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@5511 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_services/models.py
+++ gvsigol/gvsigol_services/models.py
@@ -151,6 +151,7 @@ class Layer(models.Model):
     detailed_info_enabled = models.BooleanField(default=True)
     detailed_info_button_title = models.CharField(max_length=150, null=True, blank=True, default='Detailed info')
     detailed_info_html = models.TextField(null=True, blank=True)
+    timeout = models.IntegerField(null=True, default=30000)
     
     def __unicode__(self):
         return self.name
