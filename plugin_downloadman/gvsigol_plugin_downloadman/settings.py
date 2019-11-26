bcc41ec3ac43c776422862800d9dfe9b8a256457
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2019-11-26 15:58:45 +0000

    Update settings
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@5535 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_downloadman/gvsigol_plugin_downloadman/settings.py
+++ plugin_downloadman/gvsigol_plugin_downloadman/settings.py
@@ -7,6 +7,28 @@ from gvsigol.settings import BASE_URL
 TARGET_URL = BASE_URL + '/media/downloads'
 DOWNLOADS_URL = BASE_URL + '/downloads'
 DOWNLOADS_ROOT = '/var/www/downloads'
+DOWNMAN_PACKAGING_BEHAVIOUR = 'DYNAMIC'
+
+STATISTICS=[
+{
+    'id': 'gvsigol_plugin_downloadman',
+    'count': 1,
+    'operation': 'layer_resource_downloaded',
+    'reverse_petition': False,
+    'title': 'Layer resource downloads',
+    'target_title': 'LayerResourceProxy',
+    'target_field': 'fq_title_name'
+},
+{
+    'id': 'gvsigol_plugin_downloadman',
+    'count': 2,
+    'operation': 'layer_downloaded',
+    'reverse_petition': False,
+    'title': 'Layer downloads',
+    'target_title': 'LayerProxy',
+    'target_field': 'title'
+},
+]
 
 LOCAL_PATHS_WHITELIST = ["/var/www/downloads"]
 
--- plugin_downloadman/gvsigol_plugin_downloadman/settings_tpl.py
+++ plugin_downloadman/gvsigol_plugin_downloadman/settings_tpl.py
@@ -6,4 +6,26 @@ TARGET_ROOT='##DOWNLOADMANAGER_TARGET_ROOT##'
 TARGET_URL = '##DOWNLOADMANAGER_TARGET_URL##'
 DOWNLOADS_URL = '##DOWNLOADS_URL##'
 DOWNLOADS_ROOT = '##DOWNLOADMANAGER_DOWNLOADS_ROOT##'
+DOWNMAN_PACKAGING_BEHAVIOUR = 'DYNAMIC'
+
+STATISTICS=[
+{
+    'id': 'gvsigol_plugin_downloadman',
+    'count': 1,
+    'operation': 'layer_resource_downloaded',
+    'reverse_petition': False,
+    'title': 'Layer resource downloads',
+    'target_title': 'LayerResourceProxy',
+    'target_field': 'fq_title_name'
+},
+{
+    'id': 'gvsigol_plugin_downloadman',
+    'count': 2,
+    'operation': 'layer_downloaded',
+    'reverse_petition': False,
+    'title': 'Layer downloads',
+    'target_title': 'LayerProxy',
+    'target_field': 'title'
+},
+]
 LOCAL_PATHS_WHITELIST = [##DOWNLOADMANAGER_LOCAL_PATHS_WHITELIST##]
