ffb00770e6d249a2a7344b6e273fca80b105d8d2
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2020-01-07 15:57:50 +0000

    Fix class name
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@5610 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_statistics/apps.py
+++ gvsigol/gvsigol_statistics/apps.py
@@ -3,5 +3,6 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class GvsigolSymbologyConfig(AppConfig):
+class GvsigolStatisticsConfig(AppConfig):
     name = 'gvsigol_statistics'
+    verbose_name = 'Statistics plugin'
