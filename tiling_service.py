e2b6bb22e868936cfdbe8d7ce8e8450e802827f2
nbrodin <nbrodin@216424d3-d1a8-4ca4-951f-a609719bba38>
2020-02-05 11:25:09 +0000

    Cuando las coordenadas son -180,-90,180,90 se sale de rango el cálculo del logaritmo
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol_services@5655 216424d3-d1a8-4ca4-951f-a609719bba38

--- tiling_service.py
+++ tiling_service.py
@@ -70,7 +70,10 @@ class Tiling():
         lat_rad = math.radians(lat_deg)
         n = 2.0 ** zoom
         xtile = int((lon_deg + 180.0) / 360.0 * n)
-        ytile = int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2.0 * n)
+        aux = math.tan(lat_rad) + (1 / math.cos(lat_rad))
+        if(aux == 0):
+            aux = 1
+        ytile = int((1.0 - math.log(aux) / math.pi) / 2.0 * n)
         return (xtile, ytile)
     
     def _utm2num(self, y, x, zoom):
