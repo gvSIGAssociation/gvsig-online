9384cbc06f16509d1670b68f6009ef2bb860f5ae
nbrodin <nbrodin@216424d3-d1a8-4ca4-951f-a609719bba38>
2020-02-12 10:15:03 +0000

    Error en la autorización de geoserver
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@5664 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_services/views.py
+++ gvsigol/gvsigol_services/views.py
@@ -2800,8 +2800,8 @@ def get_feature_info(request):
                             if query_layer != 'plg_catastro':
                                 if 'username' in request.session and 'password' in request.session:
                                     if request.session['username'] is not None and request.session['password'] is not None:
-                                        #auth2 = (request.session['username'], request.session['password'])
-                                        auth2 = ('admin', 'geoserver')
+                                        auth2 = (request.session['username'], request.session['password'])
+                                        #auth2 = ('admin', 'geoserver')
                                         break
                                         
     
@@ -3085,8 +3085,8 @@ def get_datatable_data(request):
             req = requests.Session()
             if 'username' in request.session and 'password' in request.session:
                 if request.session['username'] is not None and request.session['password'] is not None:
-                    #req.auth = (request.session['username'], request.session['password'])
-                    req.auth = ('admin', 'geoserver')
+                    req.auth = (request.session['username'], request.session['password'])
+                    #req.auth = ('admin', 'geoserver')
 
             print wfs_url + "?" + params
             response = req.post(wfs_url, data=values, verify=False, proxies=settings.PROXIES)
