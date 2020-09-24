a73caae7cb9d5d3a5b0665e8181301b835d38f5e
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2020-09-24 15:43:45 +0000

    Se cambia el output del tooltip de los piecharts para que muestre porcentajes
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@6016 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_charts/gvsigol_plugin_charts/static/js/lib/tools/ChartsView.js
+++ plugin_charts/gvsigol_plugin_charts/static/js/lib/tools/ChartsView.js
@@ -704,6 +704,24 @@ ChartsView.prototype.createPieChart = function(c) {
 			responsive: true,
 			legend: {
 				position: 'right',
+			},
+			tooltips: {
+				callbacks: {
+					label: function(tooltipItem, data) {
+						//get the concerned dataset
+						var dataset = data.datasets[tooltipItem.datasetIndex];
+						//calculate the total of this data set
+						var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
+						  return previousValue + currentValue;
+						});
+						//get the current items value
+						var currentValue = dataset.data[tooltipItem.index];
+						//calculate the precentage based on the total and current item, also this does a rough rounding to give a whole number
+						var percentage = Math.floor(((currentValue/total) * 100)+0.5);
+				  
+						return percentage + "%";
+					}
+				}
 			}
 		}
 	});
@@ -746,6 +764,24 @@ ChartsView.prototype.createAggregatedPieChart = function(c) {
 			responsive: true,
 			legend: {
 				position: 'right',
+			},
+			tooltips: {
+				callbacks: {
+					label: function(tooltipItem, data) {
+						//get the concerned dataset
+						var dataset = data.datasets[tooltipItem.datasetIndex];
+						//calculate the total of this data set
+						var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
+						  return previousValue + currentValue;
+						});
+						//get the current items value
+						var currentValue = dataset.data[tooltipItem.index];
+						//calculate the precentage based on the total and current item, also this does a rough rounding to give a whole number
+						var percentage = Math.floor(((currentValue/total) * 100)+0.5);
+				  
+						return percentage + "%";
+					}
+				}
 			}
 		}
 	});
--- plugin_charts/gvsigol_plugin_charts/static/js/lib/tools/SingleChart.js
+++ plugin_charts/gvsigol_plugin_charts/static/js/lib/tools/SingleChart.js
@@ -536,6 +536,24 @@ SingleChart.prototype.createPieChart = function(c) {
 			responsive: true,
 			legend: {
 				position: 'right',
+			},
+			tooltips: {
+				callbacks: {
+					label: function(tooltipItem, data) {
+						//get the concerned dataset
+						var dataset = data.datasets[tooltipItem.datasetIndex];
+						//calculate the total of this data set
+						var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
+						  return previousValue + currentValue;
+						});
+						//get the current items value
+						var currentValue = dataset.data[tooltipItem.index];
+						//calculate the precentage based on the total and current item, also this does a rough rounding to give a whole number
+						var percentage = Math.floor(((currentValue/total) * 100)+0.5);
+				  
+						return percentage + "%";
+					}
+				}
 			}
 		}
 	});
@@ -577,6 +595,24 @@ SingleChart.prototype.createAggregatedPieChart = function(c) {
 			responsive: true,
 			legend: {
 				position: 'right',
+			},
+			tooltips: {
+				callbacks: {
+					label: function(tooltipItem, data) {
+						//get the concerned dataset
+						var dataset = data.datasets[tooltipItem.datasetIndex];
+						//calculate the total of this data set
+						var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
+						  return previousValue + currentValue;
+						});
+						//get the current items value
+						var currentValue = dataset.data[tooltipItem.index];
+						//calculate the precentage based on the total and current item, also this does a rough rounding to give a whole number
+						var percentage = Math.floor(((currentValue/total) * 100)+0.5);
+				  
+						return percentage + "%";
+					}
+				}
 			}
 		}
 	});
