635bbdc8a9fe43c321c5331db542792aea5acbe9
carlesmarti <carlesmarti@216424d3-d1a8-4ca4-951f-a609719bba38>
2021-02-03 11:30:36 +0000

    initialing port
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/plugin_geoetl@6244 216424d3-d1a8-4ca4-951f-a609719bba38

---
+++ gvsigol_plugin_geoetl/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+default_app_config = 'gvsigol_plugin_geoetl.apps.GvsigolETLConfig'
---
+++ gvsigol_plugin_geoetl/apps.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+
+class GvsigolETLConfig(AppConfig):
+    name = 'gvsigol_plugin_geoetl'
+    verbose_name = "Plugin de la herramienta ETL"
+    label = "gvsigol_plugin_geoetl"
