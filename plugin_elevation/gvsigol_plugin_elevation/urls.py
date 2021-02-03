5d388ffd54e25b914bb54899f8ec87f942c602d8
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2021-02-03 11:07:01 +0000

    Adapt to Python3 / Django 2
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/branches/gvsigol_v3_prep@6240 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_elevation/gvsigol_plugin_elevation/urls.py
+++ plugin_elevation/gvsigol_plugin_elevation/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.urls import path
 from gvsigol_plugin_elevation import views as print_views
 
 urlpatterns = [
-    url(r'^elevation/get_conf/$', print_views.get_conf, name='get_conf'),
+    path('elevation/get_conf/', print_views.get_conf, name='get_conf'),
 ]
