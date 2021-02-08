8aa5f76099dd1c0373869b77e17ac1d4b9fa4799
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2021-02-08 14:02:38 +0000

    Nueva versión de desarrollo 2.8.4-dev #4692
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol@6263 216424d3-d1a8-4ca4-951f-a609719bba38

--- settings_tpl.py
+++ settings_tpl.py
@@ -28,7 +28,7 @@ from django_auth_ldap.config import LDAPSearch
 from django.utils.translation import ugettext_lazy as _
 from django.core.files.storage import FileSystemStorage
 
-GVSIGOL_VERSION = '2.8.3-dev'
+GVSIGOL_VERSION = '2.8.4-dev'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 if '__file__' in globals():
@@ -510,5 +510,6 @@ OSM_TILING_3 = '##OSM_TILING_3##'
 
 RELOAD_NODES_DELAY = 5 #EN SEGUNDOS
 
-ALLOWED_HOST_NAMES = [##ALLOWED_HOST_NAMES##]
+ALLOWED_HOST_NAMES = [##ALLOWED_HOST_NAMES##
+                      ]
 
