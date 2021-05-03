29f012816505d443aa34c365168a103fc1707724
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2021-05-03 11:23:46 +0000

    Don't serialize form_groups within layer config. Refs #4858
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/branches/gvsigol_v2.x@6520 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_services/views.py
+++ gvsigol/gvsigol_services/views.py
@@ -1560,11 +1560,7 @@ def layer_autoconfig(layer, featuretype):
 
     layer.conf = layer_conf
 
-def _parse_form_groups(json_str, fields):
-    try:
-        form_groups = json.loads(json_str)
-    except:
-        form_groups = None
+def _parse_form_groups(form_groups, fields):
     if not(isinstance(form_groups, list)) or len(form_groups) == 0:
         form_groups = [{
             'name': 'group1'
@@ -1640,8 +1636,12 @@ def layer_config(request, layer_id):
                     field['editable'] = False
             fields.append(field)
         conf['fields'] = fields
-        form_groups = _parse_form_groups(request.POST.get('form_groups', []), fields)
-        conf['form_groups'] = json.dumps(form_groups)
+        try:
+            form_groups = json.loads(request.POST.get('form_groups', []))
+        except:
+            form_groups = None
+        form_groups = _parse_form_groups(form_groups, fields)
+        conf['form_groups'] = form_groups
         layer.conf = conf
         layer.save()
 
