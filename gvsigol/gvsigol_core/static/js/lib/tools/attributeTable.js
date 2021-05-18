ad8be872df891b3f7d95770b68b36ef5c21c1137
fjp <fjp@216424d3-d1a8-4ca4-951f-a609719bba38>
2021-05-18 07:46:34 +0000

    layouts invisible in print combo should start with _ character
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@6595 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_core/static/js/lib/tools/attributeTable.js
+++ gvsigol/gvsigol_core/static/js/lib/tools/attributeTable.js
@@ -1062,11 +1062,11 @@ attributeTable.prototype.createPrintJob = function(featureType, selectedRows) {
 	$.ajax({
 		type: 'POST',
 		async: true,
-	  	url: self.printProvider.url + '/print/a4_landscape_att/report.pdf',
+	  	url: self.printProvider.url + '/print/_a4_landscape_att/report.pdf',
 	  	processData: false,
 	    contentType: 'application/json',
 	  	data: JSON.stringify({
-	  		"layout": "A4 landscape att",
+	  		"layout": "_a4_landscape_att",
 		  	"outputFormat": "pdf",
 		  	"attributes": {
 		  		"title": self.layer.title,
