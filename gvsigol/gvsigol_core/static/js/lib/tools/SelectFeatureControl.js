55a97c3f66534770a1754768052dd4a3e19c35ca
carlesmarti <carlesmarti@216424d3-d1a8-4ca4-951f-a609719bba38>
2021-05-20 15:08:27 +0000

    traducciones portugues
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/branches/gvsigol_v2.x@6627 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_core/static/js/lib/tools/SelectFeatureControl.js
+++ gvsigol/gvsigol_core/static/js/lib/tools/SelectFeatureControl.js
@@ -39,7 +39,7 @@
 	this.control = new ol.control.Toggle({	
 		html: '<i class="fa fa-mouse-pointer" ></i>',
 		className: "edit",
-		title: gettext('Select features'),
+		title: gettext('Select feature'),
 		interaction: this.interaction,
 		onToggle: function(active){
 			if (active) {
