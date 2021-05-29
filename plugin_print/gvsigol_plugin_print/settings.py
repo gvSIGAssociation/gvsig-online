cfc5d247bb94fe2771938e3162809be120a1291b
fjp <fjp@216424d3-d1a8-4ca4-951f-a609719bba38>
2021-05-29 12:10:10 +0000

    Dejar como estaba el settings para no romper nada.
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@6648 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_print/gvsigol_plugin_print/settings.py
+++ plugin_print/gvsigol_plugin_print/settings.py
@@ -3,7 +3,7 @@ APP_TITLE = 'Catálogo de metadatos'
 APP_DESCRIPTION = 'Catálogo de metadatos'
 
 PRINT_PROVIDER = {
-    'url': '/print',
+    'url': 'https://localhost/print',
     'legal_advice': 'La información y cartografía disponible en este geoportal es propiedad de ........',
     'default_scales': [10000000, 5000000, 1000000, 500000, 250000, 100000, 50000, 25000, 10000, 5000, 2000, 1000, 500]
 }
