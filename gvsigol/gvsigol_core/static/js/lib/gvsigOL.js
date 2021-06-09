87a6d8780656869cf4d2821bacf92dcba098f104
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2021-06-09 15:07:43 +0000

    Require CSRF token for server status modifying requests. Refs: #4994
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/branches/gvsigol_v2.x@6695 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_core/static/js/lib/gvsigOL.js
+++ gvsigol/gvsigol_core/static/js/lib/gvsigOL.js
@@ -580,6 +580,22 @@ function _init() {
 				box.slideUp(this.animationSpeed);
 			}
 	};
+
+	$.gvsigOL.getCsrfCookie = function () {
+		let cookieValue = null;
+		if (document.cookie && document.cookie !== '') {
+			const cookies = document.cookie.split(';');
+			for (let i = 0; i < cookies.length; i++) {
+				const cookie = cookies[i].trim();
+				// Does this cookie string begin with the name we want?
+				if (cookie.substring(0, 'csrftoken'.length + 1) === ('csrftoken' + '=')) {
+					cookieValue = decodeURIComponent(cookie.substring('csrftoken'.length + 1));
+					break;
+				}
+			}
+		}
+		return cookieValue;
+	}
 }
 
 /* ------------------
--- gvsigol/gvsigol_filemanager/urls.py
+++ gvsigol/gvsigol_filemanager/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     url(r'^$', BrowserView.as_view(), name='browser'),
     #url(r'^pbrowser/$', PopupBrowserView.as_view(), name='pbrowser'),
     url(r'^upload/$', UploadView.as_view(), name='upload'),
-    url(r'^upload/file/$', csrf_exempt(UploadFileView.as_view()), name='upload-file'),
-    url(r'^delete/file/$', csrf_exempt(DeleteFileView.as_view()), name='delete-file'),
-    url(r'^unzip/file/$', csrf_exempt(UnzipFileView.as_view()), name='unzip-file'),
+    url(r'^upload/file/$', UploadFileView.as_view(), name='upload-file'),
+    url(r'^delete/file/$', DeleteFileView.as_view(), name='delete-file'),
+    url(r'^unzip/file/$', UnzipFileView.as_view(), name='unzip-file'),
     url(r'^create/directory/$', DirectoryCreateView.as_view(), name='create-directory'),
     url(r'^export_to_database/$', ExportToDatabaseView.as_view(), name='export-to-database'),
 ]
