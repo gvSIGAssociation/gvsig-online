f3db0d08f65bbe238e6e9123d3bfcded87bf2663
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2021-09-15 15:57:21 +0000

    Nueva versión de desarrollo 2.10.8-dev #5253
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/branches/gvsigol_v2.x@6844 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol/settings_tpl.py
+++ gvsigol/gvsigol/settings_tpl.py
@@ -35,7 +35,7 @@ debug.HIDDEN_SETTINGS = re.compile(
     flags=re.IGNORECASE,
 )
 
-GVSIGOL_VERSION = '2.10.7-dev'
+GVSIGOL_VERSION = '2.10.8-dev'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 if '__file__' in globals():
