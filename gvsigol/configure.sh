cf91db3115e9f0dc89e65782eea18c4838a570ce
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2021-10-04 13:57:19 +0000

    Quote python string
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@6884 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/configure.sh
+++ gvsigol/configure.sh
@@ -265,7 +265,7 @@ function configure() {
 	echo "INFO: Replace ALLOWED_HOST_NAMES"
 	if [ -z $ALLOWED_HOST_NAMES ]; then
 		echo "WARNING: ALLOWED_HOST_NAMES is not defined. Using https://${GVSIGOL_HOST}"
-		ALLOWED_HOST_NAMES="https://${GVSIGOL_HOST}"
+		ALLOWED_HOST_NAMES="'https://${GVSIGOL_HOST}'"
 	fi
 	grep -rl "##ALLOWED_HOST_NAMES##"  | xargs sed -i "s ##ALLOWED_HOST_NAMES## $ALLOWED_HOST_NAMES g"
 	
