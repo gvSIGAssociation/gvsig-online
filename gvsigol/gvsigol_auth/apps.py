a673dffbaf81b778db430ec43a5333cfdbc66695
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2021-11-12 19:14:16 +0000

    Ensure the admin group is created and superusers belong to the admin UserGroup
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/branches/gvsigol_v2.x@6931 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_auth/apps.py
+++ gvsigol/gvsigol_auth/apps.py
@@ -9,3 +9,29 @@ class GvsigolAuthConfig(AppConfig):
         from actstream import registry
         from django.contrib.auth import get_user_model
         registry.register(get_user_model())
+        try:
+            # ensure we have a proper environment
+            self._ensure_admin_group()
+        except:
+            # Don't fail when we are migrating applications!!
+            pass
+    
+    def _ensure_admin_group(self):
+        from gvsigol_auth.models import UserGroup, UserGroupUser
+        from gvsigol_auth.utils import is_superuser
+        from django.contrib.auth.models import User 
+        try:
+            group = UserGroup.objects.get(name='admin')
+        except UserGroup.DoesNotExist:
+            group = UserGroup()
+            group.name = 'admin'
+            group.description = 'admin group'
+            group.save()
+        
+        superusers = User.objects.filter(is_superuser=True)
+        for user in superusers:
+            if UserGroupUser.objects.filter(user=user, user_group=group).count()==0:
+                ugu = UserGroupUser()
+                ugu.user = user
+                ugu.user_group = group
+                ugu.save()
