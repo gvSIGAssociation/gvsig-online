a79b7ff84f7697b31d6e69d5f0b1d8beb7c3b9bd
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2021-12-02 12:34:57 +0000

    Complete fix: only load catastro scripts and css for viewer. Refs: #4929
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol_core@6955 216424d3-d1a8-4ca4-951f-a609719bba38

--- views.py
+++ views.py
@@ -740,7 +740,8 @@ def load_project(request, project_name):
             'extra_params': json.dumps(request.GET),
             'plugins_config': plugins_config,
             'is_shared_view': False,
-            'main_page': settings.LOGOUT_PAGE_URL
+            'main_page': settings.LOGOUT_PAGE_URL,
+            'is_viewer_template': True
         }
         response = render(request, 'viewer.html', resp)
 
@@ -775,7 +776,8 @@ def load_public_project(request, project_name):
         'extra_params': json.dumps(request.GET),
         'plugins_config': plugins_config,
         'is_shared_view': False,
-        'main_page': settings.LOGOUT_PAGE_URL
+        'main_page': settings.LOGOUT_PAGE_URL,
+        'is_viewer_template': True
         }
     )
 
@@ -1370,7 +1372,8 @@ def load_shared_view(request, view_name):
             'plugins_config': plugins_config,
             'is_shared_view': True,
             'shared_view_name': shared_view.name,
-            'main_page': settings.LOGOUT_PAGE_URL
+            'main_page': settings.LOGOUT_PAGE_URL,
+            'is_viewer_template': True
             }
         )
 
