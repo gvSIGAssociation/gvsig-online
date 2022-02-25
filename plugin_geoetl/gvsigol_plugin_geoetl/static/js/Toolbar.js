29ae44b202a3c01839f9e6738b649d032b74a8a6
carlesmarti <carlesmarti@216424d3-d1a8-4ca4-951f-a609719bba38>
2022-02-25 10:33:23 +0000

    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@7031 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_geoetl/gvsigol_plugin_geoetl/static/js/Toolbar.js
+++ plugin_geoetl/gvsigol_plugin_geoetl/static/js/Toolbar.js
@@ -308,7 +308,7 @@ gvsigolETL.Toolbar = Class.extend({
 						try{
 							if (parameters[0][key]){
 								
-								if ($('#'+key+'-'+figure.id).is('select') && !key.includes('epsg') ){
+								if ($('#'+key+'-'+figure.id).is('select') && !key.includes('epsg') && !key.includes('option') ){
 									
 									if (Array.isArray(schemaold[multiIn])){
 										
