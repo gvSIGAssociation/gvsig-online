3fb99ff41ee89926f7f7edc45317597398139353
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2022-04-01 15:28:44 +0000

    API de autenticación para integración con OIDC/Keycloak. Refs: #5396
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/app_dev@7107 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol_app_dev/apps.py
+++ gvsigol_app_dev/apps.py
@@ -7,29 +7,5 @@ class GvsigolDevConfig(AppConfig):
     name = 'gvsigol_app_dev'
 
     def ready(self):
-        try:
-            # ensure we have a proper environment
-            self._ensure_admin_group()
-        except:
-            # Don't fail when we are migrating applications!!
-            pass
-    
-    def _ensure_admin_group(self):
-        from gvsigol_auth.models import UserGroup, UserGroupUser
-        from gvsigol_auth.utils import is_superuser
-        from django.contrib.auth.models import User 
-        try:
-            group = UserGroup.objects.get(name='admin')
-        except UserGroup.DoesNotExist:
-            group = UserGroup()
-            group.name = 'admin'
-            group.description = 'admin group'
-            group.save()
-        
-        superusers = User.objects.filter(is_superuser=True)
-        for user in superusers:
-            if UserGroupUser.objects.filter(user=user, user_group=group).count()==0:
-                ugu = UserGroupUser()
-                ugu.user = user
-                ugu.user_group = group
-                ugu.save()
+        pass
+
