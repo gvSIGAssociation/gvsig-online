98c9f2b4c61eaca42fb52d76c14a3e7198cd2050
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2022-04-08 12:53:47 +0000

    API stubs; define keycloak admin base clases. #5861
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@7128 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_oidc_mozilla/gvsigol_plugin_oidc_mozilla/__init__.py
+++ plugin_oidc_mozilla/gvsigol_plugin_oidc_mozilla/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 default_app_config = 'gvsigol_plugin_oidc_mozilla.apps.GvsigolOidcMozillaConfig'
 
-from .gvsigol_auth_mozilla import has_group, has_role, get_groups, get_roles, provider_logout
+from .gvsigol_auth_mozilla import *
 
