810824cd068837e719ef21511dcb030a4da99146
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2022-04-22 16:10:50 +0000

    Basic group management
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol@7168 216424d3-d1a8-4ca4-951f-a609719bba38

--- utils.py
+++ utils.py
@@ -8,3 +8,10 @@ def import_settings(module_name, globals_dict):
     except Exception as e:
         print(str(e))
         print("error importing {} module settings")
+
+def default_sorter(item):
+    """
+    Sorter to be used with sorted() and list sort() methods.
+    It sorts items by name, ignoring case.
+    """
+    return item.get('name').lower()
