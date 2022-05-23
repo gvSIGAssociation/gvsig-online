202724e65f12711791b99f83483c5005bab29908
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2022-05-23 12:18:47 +0000

    Move maintenance tasks to Celery. Refs: #5888, #5889
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol_core@7231 216424d3-d1a8-4ca4-951f-a609719bba38

--- apps.py
+++ apps.py
@@ -1,33 +1,11 @@
 
 
 from django.apps import AppConfig
-import threading
-import schedule
-import time
 import sys
 import os
 import locale
 import logging
 
-sch = schedule.default_scheduler
-job_thread = None
-
-class my_schedule_thread(threading.Thread):
-    def __init__(self, s):
-        threading.Thread.__init__(self)
-        self.daemon = True  # OK for main to exit even if instance is still running
-        self.schedule = s
-        self.continue_loop = True
-    
-    def run(self):
-        while self.continue_loop:
-            #print "############################ "+self.name+"("+str(self.ident)+") in loop with " + str(self.schedule.jobs.__len__())
-            self.schedule.run_pending()
-            #gevent.sleep(1)
-            time.sleep(5)
-            
-    def stop(self):
-        self.continue_loop = False
 
 class GvsigolCoreConfig(AppConfig):
     name = 'gvsigol_core'
@@ -42,30 +20,9 @@ class GvsigolCoreConfig(AppConfig):
 
         try:
             # ensure we have a proper environment
-            self.config_gdaltools()
-            
-            print('Scheduled tasks manager..... ON')
-            global sch, job_thread
-            current_thread = threading.current_thread()
-            for thread in threading.enumerate():
-                if thread == current_thread:
-                    continue
-                if isinstance(thread, my_schedule_thread):
-                    job_thread = thread
-            
-            if job_thread == None:
-                job_thread = my_schedule_thread(sch)
-                job_thread.start()
-            
-            print("Starting tasks....")
-            self.init_tasks()
-            
+            self.config_gdaltools()            
         except:
             pass
-        
-    def init_tasks(self):
-        from gvsigol_core import tasks
-        schedule.every().hour.do(tasks.clean_shared_views)
 
     def config_gdaltools(self):
         import gdaltools
@@ -77,30 +34,25 @@ class GvsigolCoreConfig(AppConfig):
     def output_locale_conf(self):
         logger = logging.getLogger('gvsigol')
         try:
-            logger.info("sys.stdout.encoding:")
-            logger.info(sys.stdout.encoding)
+            logger.info("sys.stdout.encoding: {}".format(sys.stdout.encoding))
         except:
             logger.info('sys.stdout.encoding not defined')
         try:
-            logger.info("locale.getpreferredencoding():")
-            logger.info(locale.getpreferredencoding())
+            logger.info("locale.getpreferredencoding(): {}".format(locale.getpreferredencoding()))
         except:
             logger.info('locale.getpreferredencoding() not defined')
 
         try:
-            logger.info("locale.getdefaultlocale():")
-            logger.info(locale.getdefaultlocale())
+            logger.info("locale.getdefaultlocale(): {}".format(locale.getdefaultlocale()))
         except:
             logger.info('locale.getdefaultlocale() not defined')
 
         try:
-            logger.info("os.environ.get('LANG'):")
-            logger.info(os.environ.get('LANG'))
+            logger.info("'LANG': {}".format(os.environ.get('LANG')))
         except:
             logger.info("os.environ.get('LANG') not defined")
 
         try:
-            logger.info("os.environ.get('LC_ALL'):")
-            logger.info(os.environ.get('LC_ALL'))
+            logger.info("'LC_ALL': {}".format(os.environ.get('LC_ALL')))
         except:
             logger.info("os.environ.get('LC_ALL') not defined")
