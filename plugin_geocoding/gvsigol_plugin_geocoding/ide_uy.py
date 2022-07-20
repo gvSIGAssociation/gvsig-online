db7eb48dfbad2d50e5cb3e6a3173351ec5895746
nbrodin <nbrodin@216424d3-d1a8-4ca4-951f-a609719bba38>
2022-07-20 06:49:12 +0000

    refs #6241
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/branches/gvsigol_v2.x@7320 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_geocoding/gvsigol_plugin_geocoding/ide_uy.py
+++ plugin_geocoding/gvsigol_plugin_geocoding/ide_uy.py
@@ -165,7 +165,7 @@ class IdeUY():
     @staticmethod   
     def get_json_from_url(url, params):
         print(url)
-        response = requests.get(url=url, params=params)
+        response = requests.get(url=url, params=params, verify=False)
         if response.status_code == 200:
             respuesta = response.content
             data = json.loads(respuesta)
