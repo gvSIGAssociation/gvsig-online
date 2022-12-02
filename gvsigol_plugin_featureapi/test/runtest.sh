feda9c6fbbd135e4931275db8122532bf15c1328
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2022-12-02 17:11:38 +0000

    Get user and pass from environment
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/plugin_featureapi@7431 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol_plugin_featureapi/test/runtest.sh
+++ gvsigol_plugin_featureapi/test/runtest.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-newman run TestLayersgvSIGOnlineAPI.json "--global-var user=$GVSIGOL_USER" "--global-var password=$GVSIGOL_PASS" -e Devel_environment.json 
+newman run TestLayersgvSIGOnlineAPI.json --global-var "user=$GVSIGOL_USER" --global-var "password=$GVSIGOL_PASS" -e Devel_environment.json 
