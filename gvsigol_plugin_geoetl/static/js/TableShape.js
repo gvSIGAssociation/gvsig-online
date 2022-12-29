3ba70fb44692bf1c6d67f99ff87b191a2b637725
carlesmarti <carlesmarti@216424d3-d1a8-4ca4-951f-a609719bba38>
2022-12-29 16:19:53 +0000

    #6605
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/plugin_geoetl@7450 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol_plugin_geoetl/static/js/TableShape.js
+++ gvsigol_plugin_geoetl/static/js/TableShape.js
@@ -7916,6 +7916,228 @@ trans_SpatialRel = draw2d.shape.layout.VerticalLayout.extend({
 
 });
 
+//// DIFFERENCE ////
+
+trans_Difference = draw2d.shape.layout.VerticalLayout.extend({
+
+	NAME: "trans_Difference",
+	
+    init : function(attr)
+    {
+    	this._super($.extend({bgColor:"#dbddde", color:"#d7d7d7", stroke:1, radius:3},attr));
+        
+      
+        this.classLabel = new draw2d.shape.basic.Label({
+            text: gettext("Difference"), 
+            stroke:1,
+            fontColor:"#ffffff",  
+            bgColor:"#71c7ec", 
+            radius: this.getRadius(), 
+            padding:10,
+            resizeable:true,
+            editor:new draw2d.ui.LabelInplaceEditor()
+        });
+        
+        var icon = new draw2d.shape.icon.Gear({
+            minWidth:13, 
+            minHeight:13, 
+            width:13, 
+            height:13, 
+            color:"#e2504c"
+        });
+
+        this.classLabel.add(icon, new draw2d.layout.locator.XYRelPortLocator(82, 8))
+
+        this.add(this.classLabel);
+
+        var ID = this.id
+
+        setColorIfIsOpened(jsonParams, this.cssClass, ID, icon)
+
+      var context = this
+
+        icon.on("click", function(){
+            setTimeout(function(){
+                try{
+                    schemas = getOwnSchemas(context.canvas, ID)
+                    schema = schemas[0]
+                    schemaOld = schemas[1]
+                }catch{ 
+                    schema=[]
+                    schemaOld =[]
+                }
+                
+                schemaEdge = passSchemaWhenInputTask(context.canvas, listLabel, ID)
+
+                if (Array.isArray(schemaEdge[0])){
+                    schemaMod = schemaEdge[0]
+                }
+                else{
+                    schemaMod = [...schemaEdge]
+                }
+
+            },100);
+            
+
+            var paramsInter = {"id": ID,
+            "parameters": [
+            {"schema": schemaMod}
+            ]}
+
+            paramsInter['schema-old'] = schemaEdge
+            paramsInter['schema'] = schemaMod
+
+
+            passSchemaToEdgeConnected(ID, listLabel, schema, context.canvas)
+
+            isAlreadyInCanvas(jsonParams, paramsInter, ID)
+
+            icon.setColor('#4682B4')
+
+        });
+
+
+            
+
+    },
+     
+    /**
+     * @method
+     * Add an entity to the db shape
+     * 
+     * @param {String} txt the label to show
+     * @param {Number} [optionalIndex] index where to insert the entity
+     */
+    addEntity: function( optionalIndex)
+    {
+	   	 var label1 =new draw2d.shape.basic.Label({
+	   	     text: gettext("Main"),
+	   	     stroke:0.2,
+	   	     radius:0,
+	   	     bgColor:"#ffffff",
+	   	     padding:{left:10, top:3, right:10, bottom:5},
+	   	     fontColor:"#107dac",
+             resizeable:true
+	   	 });
+
+	   	 var label2 =new draw2d.shape.basic.Label({
+            text: gettext("Secondary"),
+            stroke:0.2,
+            radius:0,
+            bgColor:"#ffffff",
+            padding:{left:10, top:3, right:10, bottom:5},
+            fontColor:"#107dac",
+            resizeable:true
+        });
+
+        var label3 =new draw2d.shape.basic.Label({
+            text:gettext("Output"),
+            stroke:0.2,
+            radius:0,
+            bgColor:"#ffffff",
+            padding:{left:40, top:3, right:10, bottom:5},
+            fontColor:"#107dac",
+            resizeable:true
+        });
+
+         var input = label1.createPort("input");
+         input.setName("input_"+label1.id);
+
+         var input2 = label2.createPort("input");
+         input2.setName("input_"+label2.id);
+
+	     var output= label3.createPort("output");
+         output.setName("output_"+label3.id);
+
+
+	     if($.isNumeric(optionalIndex)){
+             this.add(label1, null, optionalIndex+1);
+             this.add(label2, null, optionalIndex+1);
+             this.add(label3, null, optionalIndex+1);
+
+	     }
+	     else{
+             this.add(label1);
+             this.add(label2);
+             this.add(label3);
+         }
+         
+         listLabel.push([this.id, [input.name, input2.name], [output.name]])
+
+	     return label1, label2, label3;
+    },
+        /**
+     * @method
+     * Remove the entity with the given index from the DB table shape.<br>
+     * This method removes the entity without care of existing connections. Use
+     * a draw2d.command.CommandDelete command if you want to delete the connections to this entity too
+     * 
+     * @param {Number} index the index of the entity to remove
+     */
+    removeEntity: function(index)
+    {
+        this.remove(this.children.get(index+1).figure);
+    },
+
+    /**
+     * @method
+     * Returns the entity figure with the given index
+     * 
+     * @param {Number} index the index of the entity to return
+     */
+    getEntity: function(index)
+    {
+        return this.children.get(index+1).figure;
+    },
+     
+     /**
+      * @method
+      * Set the name of the DB table. Visually it is the header of the shape
+      * 
+      * @param name
+      */
+     setName: function(name)
+     {
+         this.classLabel.setText(name);
+         
+         return this;
+     },
+     
+     /**
+      * @method 
+      * Return an objects with all important attributes for XML or JSON serialization
+      * 
+      * @returns {Object}
+      */
+     getPersistentAttributes : getPerAttr,
+     
+     /**
+      * @method 
+      * Read all attributes from the serialized properties and transfer them into the shape.
+      *
+      * @param {Object} memento
+      * @return
+      */
+     setPersistentAttributes : function(memento)
+     {
+         this._super(memento);
+         
+         this.setName(memento.name);
+
+         if(typeof memento.entities !== "undefined"){
+             $.each(memento.entities, $.proxy(function(i,e){
+                 var entity =this.addEntity(e.text);
+                 entity.id = e.id;
+                 entity.getInputPort(0).setName("input_"+e.id);
+                 entity.getOutputPort(0).setName("output_"+e.id);
+             },this));
+         }
+
+         return this;
+     }  
+
+});
+
 /// KEEP ATTRIBUTE ////
 
 trans_KeepAttr = draw2d.shape.layout.VerticalLayout.extend({
