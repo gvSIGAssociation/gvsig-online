c14bf2fb908fb7ea6f21da35792a2c9289241674
carlesmarti <carlesmarti@216424d3-d1a8-4ca4-951f-a609719bba38>
2023-04-05 14:55:58 +0000

    refs #7079
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@7548 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_filemanager/models.py
+++ gvsigol/gvsigol_filemanager/models.py
@@ -5,9 +5,10 @@ class exports_historical(models.Model):
     time = models.CharField(max_length=30)
     file = models.CharField(max_length=250)
     status = models.CharField(max_length=25, null=True, blank=True)
-    message = models.CharField(max_length=500, null=True, blank=True)
-    redirect = models.CharField(max_length=500, null=True, blank=True)
+    message = models.TextField( null=True, blank=True)
+    redirect = models.TextField(null=True, blank=True)
     username = models.CharField(default='', max_length=30)
+    task_id = models.CharField(default='', max_length=250)
     
     def __str__(self):
         return self.name
