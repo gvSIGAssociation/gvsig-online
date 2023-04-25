fb1d74f74b3d73261cad0d277f8421efc3dbb169
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2023-04-25 15:13:59 +0000

    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@7572 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_prueba/gvsigol_plugin_prueba/api.py
+++ plugin_prueba/gvsigol_plugin_prueba/api.py
@@ -19,48 +19,60 @@
 
 @author: Javier Rodrigo <jrodrigo@scolab.es>
 '''
-
+import logging
 import json
 from django.http.response import JsonResponse
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import action
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.permissions import IsAuthenticated
 from gvsigol_plugin_baseapi.validation import HttpException
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+
+from rest_framework.response import Response
+from django.contrib.gis.db.models.functions import Distance
+from django.contrib.gis.geos import Point
 from django.core.serializers import serialize
+# from .serializers import PoiSerializer
 from gvsigol_plugin_prueba.models import Poi
-from . import service
 import json
+from django.contrib.gis.geos import GEOSGeometry
+
+logger = logging.getLogger(__name__)
+
 
 class CsrfExemptSessionAuthentication(SessionAuthentication):
     def enforce_csrf(self, request):
         return  # To not perform the csrf check previously happening
-    
+
+
 class PoiView(ListAPIView):
     serializer_class = None
     permission_classes = [AllowAny]
-    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
+    authentication_classes = (
+        CsrfExemptSessionAuthentication, BasicAuthentication)
 
-
-    @swagger_auto_schema(operation_id='get_pois', operation_summary='', 
+    @swagger_auto_schema(operation_id='get_pois', operation_summary='',
                          responses={404: "Database connection NOT found<br>User NOT found",
                                     403: "The project is not allowed to this user"})
     @action(detail=True, methods=['GET'])
     def get(self, request):
-        try: 
-            pois = serialize('geojson', Poi.objects.all(), geometry_field='geometry', fields=('name', 'description'))
-            
+        try:
+            pois = serialize('geojson', Poi.objects.all(
+            ), geometry_field='geometry', fields=('uid', 'name', 'description'))
+
             response = {
                 'pois': pois
             }
             return JsonResponse(response, safe=False)
-            
+
         except HttpException as e:
             return e.get_exception()
-    
-    @swagger_auto_schema(operation_id='save_pois', operation_summary='', 
+
+    @swagger_auto_schema(operation_id='save_pois', operation_summary='',
                          responses={404: "Database connection NOT found<br>User NOT found",
                                     403: "The project is not allowed to this user"})
     @action(detail=True, methods=['POST'], permission_classes=[AllowAny])
@@ -69,23 +81,136 @@ class PoiView(ListAPIView):
             content = get_content(request)
             json_pois = content.get('pois')
 
-            saved = service.save_pois(json_pois)
+            try:
+                Poi.objects.all().delete()
+                count=1
+                for p in json_pois:
+                    geom_4326 = GEOSGeometry(p['wkt'], srid=4326)
+                    count = count + 1
+                    poi = Poi(
+                        uid= count,
+                        name=p['name'],
+                        description=p['description'],
+                        geometry=geom_4326
+                    )
+                    poi.save()
 
-            response = {
-                'success': saved
-            }
+                response = {
+                    'success': True
+                }
+                return JsonResponse(response, safe=False)
 
-            return JsonResponse(response, safe=False)
+            except Exception as e:
+                print('EXCEPTION IN METHOD: save_pois')
+                print(str(e))
+
+                response = {
+                    'success': False
+                }
+                return JsonResponse(response, safe=False)
 
         except HttpException as e:
             return e.get_exception()
 
+    @swagger_auto_schema(operation_id='update_pois', operation_summary='',
+                         responses={404: "Database connection NOT found<br>User NOT found",
+                                    403: "The project is not allowed to this user"})
+    @action(detail=True, methods=['PUT'], permission_classes=[AllowAny])
+    def put(self, request, pk):
+
+        try:
+            poi = Poi.objects.get(id=pk)
+            content = get_content(request)
+            properties = content.get('properties')
+            poi.name = properties['name']
+            poi.description = properties['description']
+            poi.save()
+
+            response = {
+                'success': True
+            }
+            return JsonResponse(response, safe=False)
+
+        except Exception as e:
+         print('EXCEPTION IN METHOD: update_pois')
+         print(str(e))
+
+        response = {
+            'success': False
+        }
+        return JsonResponse(response, safe=False)
+      
+
+    @swagger_auto_schema(operation_id='delete_pois', operation_summary='',
+                         responses={404: "Database connection NOT found<br>User NOT found",
+                                    403: "The project is not allowed to this user"})
+    @action(detail=True, methods=['DELETE'], permission_classes=[AllowAny])
+    def delete(self, request, pk):
+
+        try:
+            poi = Poi.objects.get(id=pk)
+            poi.delete()
+            response = {
+                'success': True
+            }
+            return JsonResponse(response, safe=False)
+
+        except Exception as e:
+         print('EXCEPTION IN METHOD: delete_pois')
+         print(str(e))
+
+        response = {
+            'success': False
+        }
+        return JsonResponse(response, safe=False)
 
 def get_content(request):
     try:
-        if(request.body != ''):
+        if (request.body != ''):
             body_unicode = request.body.decode('utf-8')
             return json.loads(body_unicode)
 
     except Exception as e:
         raise HttpException(400, "Feature malformed." + format(e))
+
+
+class SearchPoiView(ListAPIView):
+    serializer_class = None
+    permission_classes = [AllowAny]
+    authentication_classes = (
+        CsrfExemptSessionAuthentication, BasicAuthentication)
+
+    @swagger_auto_schema(operation_id='search_pois', operation_summary='',
+                         responses={404: "Database connection NOT found<br>User NOT found",
+                                    403: "The project is not allowed to this user"})
+    @action(detail=True, methods=['GET'])
+    def get(self, request):
+        try:
+            # Se Obtienen los parámetros de la solicitud HTTP
+
+            #   logger.info("Recibiendo solicitud de búsqueda de Pois...")
+
+            latitude = request.query_params.get('latitude')
+            longitude = request.query_params.get('longitude')
+            radius = request.query_params.get('radius')
+
+            # logger.info(f"Parámetros de búsqueda: latitude={latitude}, longitude={longitude}, radius={radius}")
+
+            # Se Convierten las coordenadas a un objeto Point
+            location = Point(float(longitude), float(latitude))
+
+            # Se Buscan los Pois que se encuentran dentro del radio especificado
+            pois = Poi.objects.annotate(distance=Distance(
+                'geometry', location)).filter(distance__lte=radius)
+
+            # Se Serializan los Pois encontrados como GeoJSON y devolverlos en una respuesta HTTP
+            serialize_pois = serialize(
+                'geojson', pois, geometry_field='geometry', fields=('name', 'description'))
+            response = {
+                'pois': serialize_pois
+            }
+
+            return Response(response)
+
+        except HttpException as e:
+            return e.get_exception()
--- plugin_prueba/gvsigol_plugin_prueba/urls.py
+++ plugin_prueba/gvsigol_plugin_prueba/urls.py
@@ -4,4 +4,7 @@ from . import api
 urlpatterns = [
     url(r'^prueba/pois/', api.PoiView.as_view(), name='get_pois'),
     url(r'^prueba/pois/', api.PoiView.as_view(), name='save_pois'),
+    url(r'^prueba/pois/<int:pk>/', api.PoiView.as_view(), name='update_pois'),
+    url(r'^prueba/search_pois', api.SearchPoiView.as_view(), name='search_pois'),
+    url(r'^prueba/delete_pois/<int:pk>/', api.PoiView.as_view(), name='delete_pois'),
 ]
