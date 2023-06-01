a26f466162a7dd74472ff338898a4fc50b4cc1bb
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2023-06-01 16:36:22 +0000

    Ignore empty project toc_order when updating layer group. Refs: #7292
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@7615 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_core/utils.py
+++ gvsigol/gvsigol_core/utils.py
@@ -183,23 +183,23 @@ def toc_add_layergroups(toc_structure, layer_groups):
 def toc_update_layer_group(old_layergroup, old_name, new_name, title): 
     projects_by_layergroup = ProjectLayerGroup.objects.filter(layer_group=old_layergroup)
     for p in projects_by_layergroup:
-        json_toc = p.project.toc_order
-        toc = json.loads(json_toc)
-        try:
-            toc[old_name]['name'] = new_name
-            toc[old_name]['title'] = title
-            toc[new_name] = toc.pop(old_name)
-        except Exception as e:
-            if old_name == '__default_baselayergroup__':
-                toc[old_name] = {
-                    "layers": {}, 
-                    "order": 990000, 
-                    "name": old_name, 
-                    "title": title
-                }
-            pass
-        p.project.toc_order = json.dumps(toc)
-        p.project.save()
+        if p.project.toc_order:
+            toc = json.loads(p.project.toc_order)
+            try:
+                toc[old_name]['name'] = new_name
+                toc[old_name]['title'] = title
+                toc[new_name] = toc.pop(old_name)
+            except Exception as e:
+                if old_name == '__default_baselayergroup__':
+                    toc[old_name] = {
+                        "layers": {}, 
+                        "order": 990000, 
+                        "name": old_name, 
+                        "title": title
+                    }
+                pass
+            p.project.toc_order = json.dumps(toc)
+            p.project.save()
             
    
 def toc_remove_layergroups(toc_structure, layer_groups): 
