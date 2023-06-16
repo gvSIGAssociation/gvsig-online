e87db58a380a8d8d13e71a902986bbac0c1f7081
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2023-06-16 14:12:57 +0000

    Workaround for SessionRefresh middleware bug. Refs: #GESTIC-454026
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@7634 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_oidc_mozilla/gvsigol_plugin_oidc_mozilla/urls.py
+++ plugin_oidc_mozilla/gvsigol_plugin_oidc_mozilla/urls.py
@@ -1,5 +1,5 @@
 from  mozilla_django_oidc.urls import OIDCAuthenticateClass
-from django.urls import path
+from django.urls import path, include
 from mozilla_django_oidc.views import OIDCLogoutView
 from gvsigol_plugin_oidc_mozilla import views
 
@@ -7,4 +7,7 @@ urlpatterns = [
     path('authenticate/', OIDCAuthenticateClass.as_view(), name='gvsigol_authenticate_user'),
     path('callback/', views.GvsigolOIDCAuthenticationCallbackView.as_view(), name='oidc_authentication_callback'),
     path('logout/', OIDCLogoutView.as_view(), name='gvsigol_logout_user'),
+    # not used, but required by SessionRefresh middleware
+    path('oidc/authenticate/', OIDCAuthenticateClass.as_view(), name='oidc_authentication_init'),
+    path('oidc/logout/', OIDCLogoutView.as_view(), name='oidc_logout'),
 ]
