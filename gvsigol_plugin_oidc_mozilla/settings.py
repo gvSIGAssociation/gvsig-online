adb532e258d77f10dbfca6fc8dd687ee4ac90c7a
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2023-06-26 14:58:17 +0000

    Parametrize password management UI. Refs: #GESTIC-454026
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/plugin_oidc_mozilla@7648 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol_plugin_oidc_mozilla/settings.py
+++ gvsigol_plugin_oidc_mozilla/settings.py
@@ -12,11 +12,12 @@ OIDC_OP_LOGOUT_URL_METHOD = GVSIGOL_AUTH_BACKEND + ".provider_logout"
 OIDC_RP_SIGN_ALGO = 'RS256'
 OIDC_STORE_ACCESS_TOKEN = True
 OIDC_STORE_ID_TOKEN = True
-OIDC_RP_SCOPES = 'openid email username geoserver-client'
+OIDC_RP_SCOPES = 'openid email username geoserver-client-scope'
 
 
 KEYCLOAK_ADMIN_CLIENT_ID = 'gvsigonline-auth-admin'
 KEYCLOAK_ADMIN_CLIENT_SECRET = 'af17d120-f91a-4d05-bbae-411e9359ca78'
 OIDC_EXEMPT_URLS = ['gvsigol_authenticate_user', 'gvsigol_logout_user']
 LOGIN_REDIRECT_URL_FAILURE = 'index'
+#MANAGE_PASSWORD_URL = OIDC_OP_BASE_URL + "/realms/" + OIDC_OP_REALM_NAME + "/account/#/security/signingin"
 
