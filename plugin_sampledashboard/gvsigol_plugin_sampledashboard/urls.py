e67c35bb837271aa2d549fd98dc06952757b0655
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2023-06-26 12:10:03 +0000

    Use recommended django url declarations
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@7645 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_sampledashboard/gvsigol_plugin_sampledashboard/urls.py
+++ plugin_sampledashboard/gvsigol_plugin_sampledashboard/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from gvsigol_plugin_sampledashboard import views
 
 urlpatterns = [
-    url(r'^sampledashboard/get_conf/$', views.get_conf, name='get_conf'),
-    url(r'^sampledashboard/sampledashboard_list/$', views.sampledashboard_list, name='sampledashboard_list'),
-    url(r'^sampledashboard/sampledashboard_add/$', views.sampledashboard_add, name='sampledashboard_add'),
-    url(r'^sampledashboard/sampledashboard_delete/$', views.sampledashboard_delete, name='sampledashboard_delete'),
-    url(r'^sampledashboard/sampledashboard_update/$', views.sampledashboard_update, name='sampledashboard_update'),
-    url(r'^sampledashboard/sampledashboard_update/(?P<lgid>[0-9]+)/$', views.sampledashboard_update, name='sampledashboard_update'),
+    path('sampledashboard/get_conf/', views.get_conf, name='get_conf'),
+    path('sampledashboard/sampledashboard_list/', views.sampledashboard_list, name='sampledashboard_list'),
+    path('sampledashboard/sampledashboard_add/', views.sampledashboard_add, name='sampledashboard_add'),
+    path('sampledashboard/sampledashboard_delete/', views.sampledashboard_delete, name='sampledashboard_delete'),
+    path('sampledashboard/sampledashboard_update/', views.sampledashboard_update, name='sampledashboard_update'),
+    path('sampledashboard/sampledashboard_update/<int:lgid>/', views.sampledashboard_update, name='sampledashboard_update'),
 ]
