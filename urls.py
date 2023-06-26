140a8434092051ef78d50ca0c0c64ad48a959833
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2023-06-26 14:58:17 +0000

    Parametrize password management UI. Refs: #GESTIC-454026
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol_auth@7648 216424d3-d1a8-4ca4-951f-a609719bba38

--- urls.py
+++ urls.py
@@ -18,26 +18,6 @@ else:
 
 urlpatterns = LOGIN_PATHS + [
     path('rest_session_login/', auth_views.rest_session_login, name='rest_session_login'),
-    
-    #path('password_change/done/', auth_views.password_change_done, name='password_change_done'),
-    path('reset/<int:user_id>/<uid>/<token>/', auth_views.password_reset_confirmation, name='password_reset_confirmation'),
-    path('reset/done/', auth_views.password_reset_complete, name='password_reset_confirmation_complete'),
-    path('password_update/', auth_views.password_update, name='password_update'),
-    path('password_reset/', auth_views.password_reset, name='password_reset'),
-    path('password_reset_success/', auth_views.password_reset_success, name='password_reset_success'),
-    
-    path('user_list/', auth_views.user_list, name='user_list'),
-    path('user_add/', auth_views.user_add, name='user_add'),
-    path('user_update/<uid>/', auth_views.user_update, name='user_update'),
-    path('user_delete/<uid>/', auth_views.user_delete, name='user_delete'),
-
-    path('group_list/', auth_views.group_list, name='group_list'),
-    path('group_add/', auth_views.group_add, name='group_add'),
-    path('group_delete/<group_name>/', auth_views.group_delete, name='group_delete'),
-
-    path('role_list/', auth_views.role_list, name='role_list'),
-    path('role_add/', auth_views.role_add, name='role_add'),
-    path('role_delete/<role_name>/', auth_views.role_delete, name='role_delete'),
 
     path('api-token-auth/', obtain_jwt_token, name='api-token-auth'),
     path('api-token-refresh/', refresh_jwt_token, name='api-token-refresh'),
@@ -48,3 +28,28 @@ urlpatterns = LOGIN_PATHS + [
     path('get-roles/', auth_views.get_roles, name='auth-get-roles'),
     path('get-groups/', auth_views.get_groups, name='auth-get-groups')
 ]
+
+if settings.AUTH_DASHBOARD_UI:
+    urlpatterns = urlpatterns + [
+        path('user_list/', auth_views.user_list, name='user_list'),
+        path('user_add/', auth_views.user_add, name='user_add'),
+        path('user_update/<uid>/', auth_views.user_update, name='user_update'),
+        path('user_delete/<uid>/', auth_views.user_delete, name='user_delete'),
+
+        path('group_list/', auth_views.group_list, name='group_list'),
+        path('group_add/', auth_views.group_add, name='group_add'),
+        path('group_delete/<group_name>/', auth_views.group_delete, name='group_delete'),
+
+        path('role_list/', auth_views.role_list, name='role_list'),
+        path('role_add/', auth_views.role_add, name='role_add'),
+        path('role_delete/<role_name>/', auth_views.role_delete, name='role_delete'),
+    ]
+MANAGE_PASSWORD_URL = getattr(settings, 'MANAGE_PASSWORD_URL', None)
+if not MANAGE_PASSWORD_URL:
+    urlpatterns = urlpatterns + [
+        path('reset/<int:user_id>/<uid>/<token>/', auth_views.password_reset_confirmation, name='password_reset_confirmation'),
+        path('reset/done/', auth_views.password_reset_complete, name='password_reset_confirmation_complete'),
+        path('password_update/', auth_views.password_update, name='password_update'),
+        path('password_reset/', auth_views.password_reset, name='password_reset'),
+        path('password_reset_success/', auth_views.password_reset_success, name='password_reset_success')
+    ]
