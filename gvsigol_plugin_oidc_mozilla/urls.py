4f49c7ccfe7b2483f882f3960a75c5f60e9e21f6
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2023-06-28 08:32:42 +0000

    Gestión de contraseñas adaptada al nuevo auth_backend y disponible sólo según variables de configuración. Permitir certificados autofirmados según variable de configuración. Definir API user_update en auth_backend. Refs: #GESTIC-454026
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/plugin_oidc_mozilla@7651 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol_plugin_oidc_mozilla/urls.py
+++ gvsigol_plugin_oidc_mozilla/urls.py
@@ -6,7 +6,7 @@ from gvsigol_plugin_oidc_mozilla import views
 urlpatterns = [
     path('authenticate/', OIDCAuthenticateClass.as_view(), name='gvsigol_authenticate_user'),
     path('callback/', views.GvsigolOIDCAuthenticationCallbackView.as_view(), name='oidc_authentication_callback'),
-    path('logout/', OIDCLogoutView.as_view(), name='gvsigol_logout_user'),
+    path('logout/', views.GvsigolOIDCLogoutView.as_view(), name='gvsigol_logout_user'),
     # not used, but required by SessionRefresh middleware
     path('oidc/authenticate/', OIDCAuthenticateClass.as_view(), name='oidc_authentication_init'),
     path('oidc/logout/', OIDCLogoutView.as_view(), name='oidc_logout'),
