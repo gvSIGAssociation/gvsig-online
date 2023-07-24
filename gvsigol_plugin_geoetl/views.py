a9b9e7354a4f5733af861aa6d86d46eb41a052e6
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2023-07-24 10:13:49 +0000

    Fix username value. Refs: #7727
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/plugin_geoetl@7697 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol_plugin_geoetl/views.py
+++ gvsigol_plugin_geoetl/views.py
@@ -597,14 +597,14 @@ def etl_read_canvas(request):
                 if ws.can_execute(request):
                     run_canvas_background.apply_async(kwargs = {'jsonCanvas': jsonCanvas, 
                                                                 'id_ws': id_ws, 
-                                                                'username': request.user.user.name, 
+                                                                'username': request.user.name, 
                                                                 'parameters': params,
                                                                 'concat': concat})
                     
             else: # executing a workspace that has not been saved yet
                 run_canvas_background.apply_async(kwargs = {'jsonCanvas': jsonCanvas, 
                                                             'id_ws': id_ws, 
-                                                            'username': request.user.user.name,
+                                                            'username': request.user.name,
                                                             'parameters': params,
                                                             'concat': concat})
 
