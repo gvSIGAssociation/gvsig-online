ca64bf6d85339794b6f9885e8fd7a8df67bdf110
jvhigon <jvhigon@216424d3-d1a8-4ca4-951f-a609719bba38>
2023-10-30 21:56:54 +0000

    update settings for reading envrionment refs #8179
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@7817 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_geoetl/gvsigol_plugin_geoetl/settings.py
+++ plugin_geoetl/gvsigol_plugin_geoetl/settings.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
+import environ
+import os
+import logging
+from django.conf import settings
+
+
+LOGGER = logging.getLogger('gvsigol')
+
 ETL_URL = '/etlurl'
 
 URL_GEOCODER = {
@@ -6,21 +14,24 @@ URL_GEOCODER = {
     'icv-reverse': "http://descargas.icv.gva.es/server_api/geocodificador/geocoder.php?&x=%s&y=%s"
 }
 
-try:
-    from gvsigol import settings_passwords
-    USER = settings_passwords.DB_USER_DEVEL
-    PASS = settings_passwords.DB_PW_DEVEL
-except:
-    USER = 'postgres'
-    PASS = 'postgres'
-
-
+env_plugin_geoetl = environ.Env(
+    ETL_DB_HOST=(str,settings.DATABASES['default']['HOST']),
+    ETL_DB_PORT=(str,settings.DATABASES['default']['PORT']),
+    ETL_DB_NAME=(str,settings.DATABASES['default']['NAME']),
+    ETL_DB_USER=(str,settings.DATABASES['default']['USER']),
+    ETL_DB_PASSWD=(str,settings.DATABASES['default']['PASSWORD'])
+)
 
 GEOETL_DB = {
-    'host': 'localhost',
-    'port': '5432',
-    'database': 'gvsigonline',
-    'user': USER,
-    'password': PASS,
+    'host': env_plugin_geoetl('ETL_DB_HOST'),
+    'port': env_plugin_geoetl('ETL_DB_PORT'),
+    'database': env_plugin_geoetl('ETL_DB_NAME'),
+    'user': env_plugin_geoetl('ETL_DB_USER'),
+    'password': env_plugin_geoetl('ETL_DB_PASSWD'),
     'schema': 'ds_plugin_geoetl'
 }
+
+LOGGER.info("ETL_DB_HOST= %s",GEOETL_DB['host'])
+LOGGER.info("ETL_DB_PORT= %s",GEOETL_DB['port'])
+LOGGER.info("ETL_DB_NAME= %s",GEOETL_DB['database'])
+LOGGER.info("ETL_DB_USER= %s",GEOETL_DB['user'])
