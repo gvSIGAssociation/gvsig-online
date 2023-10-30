6c4d5685d6b0036b49bc3c1778cc5a1eae4f2a3f
jvhigon <jvhigon@216424d3-d1a8-4ca4-951f-a609719bba38>
2023-10-30 17:09:51 +0000

    read core settings from environment refs #8179
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@7814 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/manage.py
+++ gvsigol/manage.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-#reload(sys)
-#sys.setdefaultencoding('ISO-8859-1')
-
-if __name__ == "__main__":
+def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gvsigol.settings")
     # CMI: to correctly use it from command line
     #sys.path.append("/home/cesar/projects/scolab/gvsig-online/wsgol2/app_dev")
     # END CMI
+    print('INFO: Setting DJANGO_SETTINGS_MODULE to gvsigol.settings')
 
-    from django.core.management import execute_from_command_line
-
+    if os.environ.get("DEBUG_REMOTE") and os.environ.get("DEBUG_REMOTE")=='True':
+        if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+            try:
+                import debugpy
+                debugpy.listen(("0.0.0.0", 6000))                
+                print('INFO: Start the VS Code debugger now, waiting...')                
+                debugpy.wait_for_client()
+                print('INFO: Ready for debugging on port 6000....') 
+            except:
+                print('WARNING: Problem loading debugpy.') 
+                pass
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "ERROR: Couldn't import Django. Check PYTHONPATH and virtualenv"
+        ) from exc
     execute_from_command_line(sys.argv)
 
+
+if __name__ == '__main__':
+    main()
