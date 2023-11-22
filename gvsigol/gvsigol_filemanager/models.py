5108032dd75c414ea17c070c85d848901d86ea6f
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2023-11-22 11:34:18 +0000

    Fix database export for users having long usernames. Refs: #8205
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@7865 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_filemanager/models.py
+++ gvsigol/gvsigol_filemanager/models.py
@@ -3,11 +3,11 @@ from django.db import models
 class exports_historical(models.Model):
 
     time = models.CharField(max_length=30)
-    file = models.CharField(max_length=250)
+    file = models.TextField()
     status = models.CharField(max_length=25, null=True, blank=True)
     message = models.TextField( null=True, blank=True)
     redirect = models.TextField(null=True, blank=True)
-    username = models.CharField(default='', max_length=30)
+    username = models.TextField(default='')
     task_id = models.CharField(default='', max_length=250)
     
     def __str__(self):
