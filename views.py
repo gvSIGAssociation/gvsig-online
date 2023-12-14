8a98a40c6d8e93994405029f9eb30f607e493278
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2023-12-14 13:15:53 +0000

    Fix nif length. Refs: #8474
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol_services@7897 216424d3-d1a8-4ca4-951f-a609719bba38

--- views.py
+++ views.py
@@ -5398,12 +5398,13 @@ def list_datastores_in_db(request):
 
 def test_dnie(request):
     cert_s_dn = request.META.get('HTTP_X_SSL_CLIENT_S_DN')
+    print(request.META.get('HTTP_X_SSL_CLIENT_VERIFY'))
     if cert_s_dn:
         nif = ''
         for i in cert_s_dn.split(","):
             if i.startswith("serialNumber="):
                 nif = i[len("serialNumber="):]
-                if len(nif) != 8:
+                if len(nif) != 9:
                     if len(nif) == 15:
                         prefix = 'IDCES-' # prefijo FNMT
                         if nif.startswith(prefix):
