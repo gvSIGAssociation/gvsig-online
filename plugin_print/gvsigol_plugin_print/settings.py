74ed26594936e83815fba31ae0cfd3c6b24477a3
jvhigon <jvhigon@216424d3-d1a8-4ca4-951f-a609719bba38>
2023-12-27 16:54:02 +0000

    environment variables conf refs #7933
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@7913 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_print/gvsigol_plugin_print/settings.py
+++ plugin_print/gvsigol_plugin_print/settings.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
-APP_TITLE = 'Catálogo de metadatos'
-APP_DESCRIPTION = 'Catálogo de metadatos'
+import environ
+import os
+
+print('INFO: Loading plugin print.')
+
+env_plugin_print = environ.Env(
+    PRINT_URL=(str,'/print'),
+    PRINT_LEGAL_ADVICE=(str,'La información y cartografía disponible en este geoportal es propiedad de ........')
+)
 
 PRINT_PROVIDER = {
-    'url': '/print',
-    'legal_advice': 'La información y cartografía disponible en este geoportal es propiedad de ........',
+    'url': env_plugin_print('PRINT_URL'),
+    'legal_advice': env_plugin_print('PRINT_LEGAL_ADVICE'),
     'default_scales': [10000000, 5000000, 1000000, 500000, 250000, 100000, 50000, 25000, 10000, 5000, 2000, 1000, 500]
 }
