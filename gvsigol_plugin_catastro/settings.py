51d51d730847a795d3e2f5fac10201f272e0e398
jvhigon <jvhigon@216424d3-d1a8-4ca4-951f-a609719bba38>
2024-02-24 06:07:57 +0000

    plugin_catastro settings.py refactor in order to use environment variables refs #7933
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/plugin_catastro@7992 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol_plugin_catastro/settings.py
+++ gvsigol_plugin_catastro/settings.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
-#URL_CATASTRO = 'http://localhost/ctt'
+
 # wfs inspire URL_CATASTRO = 'https://ovc.catastro.meh.es/INSPIRE/wfsCP.aspx'
-URL_CATASTRO_INSPIRE = 'https://ovc.catastro.meh.es/INSPIRE/wfsCP.aspx'
+
 # inspire layers URL_CATASTRO = 'http://ovc.catastro.meh.es/cartografia/INSPIRE/spadgcwms.aspx'
-URL_CATASTRO = 'http://ovc.catastro.meh.es/Cartografia/WMS/ServidorWMS.aspx'
-
 # Vieja, ya no funciona
-#URL_API_CATASTRO = 'https://ovc.catastro.meh.es/ovcservweb/OVCSWLocalizacionRC'
-
+# URL_API_CATASTRO = 'https://ovc.catastro.meh.es/ovcservweb/OVCSWLocalizacionRC'
 # Nueva API JSON
 # URL_API_CATASTRO = 'https://ovc.catastro.meh.es/OVCServWeb/OVCWcfCallejero/COVCCallejero.svc/json/'
-
 # Nueva API REST (xml, es más parecida al servicio viejo)
-URL_API_CATASTRO = 'https://ovc.catastro.meh.es/OVCServWeb/OVCWcfCallejero'
+#URL_API_CATASTRO = 'https://ovc.catastro.meh.es/OVCServWeb/OVCWcfCallejero'
+
+
+import environ
+
+print('INFO: Loading plugin catastro.')
+
+env_catastro = environ.Env(
+    URL_CATASTRO=(str,'http://ovc.catastro.meh.es/Cartografia/WMS/ServidorWMS.aspx'),
+    URL_API_CATASTRO=(str,'https://ovc.catastro.meh.es/OVCServWeb/OVCWcfCallejero'),
+    URL_CATASTRO_INSPIRE=(str,'https://ovc.catastro.meh.es/INSPIRE/wfsCP.aspx')
+)
+URL_CATASTRO=env_catastro('URL_CATASTRO')
+URL_API_CATASTRO=env_catastro('URL_API_CATASTRO')
+URL_CATASTRO_INSPIRE=env_catastro('URL_CATASTRO_INSPIRE')
