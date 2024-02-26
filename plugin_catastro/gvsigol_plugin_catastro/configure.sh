552f84391c62103d96619b0a9fd0a32e86423b0d
ggimenez <ggimenez@216424d3-d1a8-4ca4-951f-a609719bba38>
2024-02-26 11:32:09 +0000

    Add attribute URL_CATASTRO_INSPIRE
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@7999 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_catastro/gvsigol_plugin_catastro/configure.sh
+++ plugin_catastro/gvsigol_plugin_catastro/configure.sh
@@ -19,3 +19,9 @@ fi
 
 grep -rl "##URL_API_CATASTRO##" | xargs sed -i "s ##URL_API_CATASTRO## $URL_API_CATASTRO g"
 
+if [ -z $URL_CATASTRO_INSPIRE ]; then
+			URL_CATASTRO_INSPIRE="https://ovc.catastro.meh.es/INSPIRE/wfsCP.aspx"
+			echo "WARNING: URL_CATASTRO_INSPIRE is not defined, using default value $URL_CATASTRO_INSPIRE"
+fi
+
+grep -rl "##URL_CATASTRO_INSPIRE##" | xargs sed -i "s ##URL_CATASTRO_INSPIRE## $URL_CATASTRO_INSPIRE g"
