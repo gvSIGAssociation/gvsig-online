15d36d567d3f2e21d11c0caea97ba5bdb8e4c87b
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2024-04-03 15:09:51 +0000

    Rename services_base import as basetypes; get admin and staff roles from settings. Refs: #9047, #8921
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@8083 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_oidc_mozilla/gvsigol_plugin_oidc_mozilla/settings.py
+++ plugin_oidc_mozilla/gvsigol_plugin_oidc_mozilla/settings.py
@@ -18,6 +18,11 @@ LOGGER.info('Loading plugin  plugin_oidc_mozilla.')
 env_plugin_oidc_mozilla = environ.Env(
     OIDC_OP_BASE_URL=(str, '/auth'), 
     OIDC_OP_REALM_NAME=(str,'master'),
+    OIDC_OP_AUTHORIZATION_ENDPOINT=(str, None),
+    OIDC_OP_TOKEN_ENDPOINT=(str, None),
+    OIDC_OP_JWKS_ENDPOINT=(str, None),
+    OIDC_OP_LOGOUT_ENDPOINT=(str, None),
+    OIDC_OP_USER_ENDPOINT=(str, None),
     OIDC_RP_CLIENT_ID=(str),
     OIDC_RP_CLIENT_SECRET=(str),
     KEYCLOAK_ADMIN_CLIENT_ID=(str),
@@ -26,7 +31,10 @@ env_plugin_oidc_mozilla = environ.Env(
     MANAGE_PASSWORD_URL=(str,''),
     OIDC_GVSIGOL_CONFIG_MODULE=(str,''),
     OIDC_GVSIGOL_EMAIL_LOGIN=(bool,True),
-    ALLOW_LOGOUT_GET_METHOD=(bool,True)
+    ALLOW_LOGOUT_GET_METHOD=(bool,True),
+    OIDC_RP_SIGN_ALGO=(str,'RS256'),
+    GVSIGOL_SUPERUSER_ROLE=(str,'GVSIGOL_DJANGO_SUPERUSER'),
+    GVSIGOL_STAFF_ROLE=(str, 'GVSIGOL_DJANGO_STAFF')
  )
 OIDC_OP_BASE_URL = env_plugin_oidc_mozilla('OIDC_OP_BASE_URL')
 OIDC_OP_REALM_NAME = env_plugin_oidc_mozilla('OIDC_OP_REALM_NAME')
@@ -35,14 +43,29 @@ OIDC_RP_CLIENT_SECRET = env_plugin_oidc_mozilla('OIDC_RP_CLIENT_SECRET')
 KEYCLOAK_ADMIN_CLIENT_ID = env_plugin_oidc_mozilla('KEYCLOAK_ADMIN_CLIENT_ID')
 KEYCLOAK_ADMIN_CLIENT_SECRET = env_plugin_oidc_mozilla('KEYCLOAK_ADMIN_CLIENT_SECRET')
 OIDC_OP_REALM_BASE_URL= env_plugin_oidc_mozilla('OIDC_OP_BASE_URL')+ '/realms/' + env_plugin_oidc_mozilla('OIDC_OP_REALM_NAME')
-OIDC_OP_AUTHORIZATION_ENDPOINT=OIDC_OP_REALM_BASE_URL + '/protocol/openid-connect/auth'
-OIDC_OP_TOKEN_ENDPOINT=OIDC_OP_REALM_BASE_URL+ '/protocol/openid-connect/token'
-OIDC_OP_JWKS_ENDPOINT=OIDC_OP_REALM_BASE_URL+ '/protocol/openid-connect/certs'
-OIDC_OP_LOGOUT_ENDPOINT=OIDC_OP_REALM_BASE_URL+ '/protocol/openid-connect/logout'
-OIDC_OP_USER_ENDPOINT=OIDC_OP_REALM_BASE_URL+ '/protocol/openid-connect/userinfo'
+if env_plugin_oidc_mozilla('OIDC_OP_AUTHORIZATION_ENDPOINT'):
+    OIDC_OP_AUTHORIZATION_ENDPOINT=env_plugin_oidc_mozilla('OIDC_OP_AUTHORIZATION_ENDPOINT')
+else:
+    OIDC_OP_AUTHORIZATION_ENDPOINT=OIDC_OP_REALM_BASE_URL + '/protocol/openid-connect/auth'
+if env_plugin_oidc_mozilla('OIDC_OP_TOKEN_ENDPOINT'):
+    OIDC_OP_TOKEN_ENDPOINT=env_plugin_oidc_mozilla('OIDC_OP_TOKEN_ENDPOINT')
+else:
+    OIDC_OP_TOKEN_ENDPOINT=OIDC_OP_REALM_BASE_URL+ '/protocol/openid-connect/token'
+if env_plugin_oidc_mozilla('OIDC_OP_JWKS_ENDPOINT'):
+    OIDC_OP_JWKS_ENDPOINT=env_plugin_oidc_mozilla('OIDC_OP_JWKS_ENDPOINT')
+else:
+    OIDC_OP_JWKS_ENDPOINT=OIDC_OP_REALM_BASE_URL+ '/protocol/openid-connect/certs'
+if env_plugin_oidc_mozilla('OIDC_OP_LOGOUT_ENDPOINT'):
+    OIDC_OP_LOGOUT_ENDPOINT=env_plugin_oidc_mozilla('OIDC_OP_LOGOUT_ENDPOINT')
+else:
+    OIDC_OP_LOGOUT_ENDPOINT=OIDC_OP_REALM_BASE_URL+ '/protocol/openid-connect/logout'
+if env_plugin_oidc_mozilla('OIDC_OP_USER_ENDPOINT'):
+    OIDC_OP_USER_ENDPOINT=env_plugin_oidc_mozilla('OIDC_OP_USER_ENDPOINT')
+else:
+    OIDC_OP_USER_ENDPOINT=OIDC_OP_REALM_BASE_URL+ '/protocol/openid-connect/userinfo'
 #OIDC_OP_LOGOUT_ENDPOINT = ''
 OIDC_OP_LOGOUT_URL_METHOD = "gvsigol_plugin_oidc_mozilla.provider_logout"
-OIDC_RP_SIGN_ALGO = 'RS256'
+OIDC_RP_SIGN_ALGO = env_plugin_oidc_mozilla('OIDC_RP_SIGN_ALGO')
 OIDC_STORE_ACCESS_TOKEN = True
 OIDC_STORE_ID_TOKEN = True
 OIDC_RP_SCOPES = env_plugin_oidc_mozilla('OIDC_RP_SCOPES')
@@ -52,8 +75,11 @@ LOGIN_REDIRECT_URL_FAILURE = 'index'
 OIDC_GVSIGOL_CONFIG_MODULE = env_plugin_oidc_mozilla('OIDC_GVSIGOL_CONFIG_MODULE')
 OIDC_GVSIGOL_EMAIL_LOGIN = env_plugin_oidc_mozilla('OIDC_GVSIGOL_EMAIL_LOGIN')
 ALLOW_LOGOUT_GET_METHOD = env_plugin_oidc_mozilla('ALLOW_LOGOUT_GET_METHOD')
-
+#OIDC_AUTH_REQUEST_EXTRA_PARAMS= {"app": "LIBRA"}
+OIDC_USE_PKCE = True
 #DJANGO_AUTHENTICATION_BACKENDS="'gvsigol_plugin_oidc_mozilla.oidc.GvsigolOIDCAuthenticationBackend',"
+GVSIGOL_SUPERUSER_ROLE = env_plugin_oidc_mozilla('GVSIGOL_SUPERUSER_ROLE')
+GVSIGOL_STAFF_ROLE = env_plugin_oidc_mozilla('GVSIGOL_STAFF_ROLE')
 
 LOGGER.info('OIDC_OP_BASE_URL=%s',OIDC_OP_BASE_URL)
 LOGGER.info('OIDC_OP_REALM_NAME=%s', OIDC_OP_REALM_NAME)
@@ -68,6 +94,8 @@ LOGGER.info('OIDC_OP_LOGOUT_URL_METHOD=%s', OIDC_OP_LOGOUT_URL_METHOD)
 LOGGER.info('OIDC_RP_SIGN_ALGO=%s', OIDC_RP_SIGN_ALGO)
 LOGGER.info('OIDC_OP_USER_ENDPOINT=%s', OIDC_OP_USER_ENDPOINT)
 LOGGER.info('OIDC_RP_SCOPES=%s', OIDC_RP_SCOPES)
+LOGGER.info('GVSIGOL_SUPERUSER_ROLE=%s', GVSIGOL_SUPERUSER_ROLE)
+LOGGER.info('GVSIGOL_STAFF_ROLE=%s', GVSIGOL_STAFF_ROLE)
 
 # TODO: show only in debug mode
 LOGGER.info('OIDC_RP_CLIENT_ID=%s', OIDC_RP_CLIENT_ID)
