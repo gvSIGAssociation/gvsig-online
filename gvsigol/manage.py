550586c3ca669ae90a19137585640f5a9c5a3a5f
jvhigon <jvhigon@216424d3-d1a8-4ca4-951f-a609719bba38>
2024-08-10 17:54:43 +0000

    problema autoreload debugpy refs #10463
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@8209 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/manage.py
+++ gvsigol/manage.py
@@ -3,6 +3,18 @@
 import os
 import sys
 
+def initialize_debugger():
+    import debugpy
+    
+    # RUN_MAIN envvar is set by the reloader to indicate that this is the 
+    # actual thread running Django. This code is in the parent process and
+    # initializes the debugger
+    if not os.getenv("RUN_MAIN"):
+        debugpy.listen(("0.0.0.0", 6000))
+        sys.stdout.write("Start the VS Code debugger now, waiting...\n")
+        debugpy.wait_for_client()
+        sys.stdout.write("Debugger attached, starting server...\n")
+
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gvsigol.settings")
     # CMI: to correctly use it from command line
@@ -10,23 +22,23 @@ def main():
     # END CMI
     print('INFO manage.py: Setting DJANGO_SETTINGS_MODULE to gvsigol.settings')
 
-    if os.environ.get("DEBUG_REMOTE") and os.environ.get("DEBUG_REMOTE")=='True':
-        if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
-            try:
-                import debugpy
-                debugpy.listen(("0.0.0.0", 6000))                
-                print('INFO: Start the VS Code debugger now, waiting...')                
-                debugpy.wait_for_client()
-                print('INFO: Ready for debugging on port 6000....') 
-            except:
-                print('WARNING: Problem loading debugpy.') 
-                pass
+    # if os.environ.get("DEBUG_REMOTE") and os.environ.get("DEBUG_REMOTE")=='True':
+    #     if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+    #         try:
+    #             import debugpy
+    #             debugpy.listen(("0.0.0.0", 6000))                
+    #             print('INFO: Start the VS Code debugger now, waiting...')                
+    #             debugpy.wait_for_client()
+    #             print('INFO: Ready for debugging on port 6000....') 
+    #         except:
+    #             print('WARNING: Problem loading debugpy.') 
+    #             pass
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
-            "ERROR: Couldn't import Django. Check PYTHONPATH and virtualenv"
-        ) from exc
+        raise ImportError("ERROR: Couldn't import Django. Check PYTHONPATH and virtualenv") from exc
+    if os.environ.get("DEBUG_REMOTE") and os.environ.get("DEBUG_REMOTE")=='True':
+        initialize_debugger()
     execute_from_command_line(sys.argv)
 
 
