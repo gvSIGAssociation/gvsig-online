ce5a589c96f616c5f688fee5a826cbf25704ff1e
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2024-12-11 15:46:36 +0000

    Add button to download layer resources. Refs: #11352
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol_core@8322 216424d3-d1a8-4ca4-951f-a609719bba38

--- static/js/lib/widgets/downloadManagerClientBase.js
+++ static/js/lib/widgets/downloadManagerClientBase.js
@@ -54,7 +54,7 @@ DownloadManagerUI.prototype.layerDirectDownloads = function(layer) {
 			var gmlLink = layer.wfs_url + '?service=WFS&version=1.1.0&request=GetFeature&outputFormat=GML3&typeName=' + layer.layer_name;
 			var csvLink = layer.wfs_url + '?service=WFS&version=1.1.0&request=GetFeature&outputFormat=csv&typeName=' + layer.layer_name;
 			var geojsonLink = layer.wfs_url + '?service=WFS&version=1.1.0&request=GetFeature&outputFormat=json&typeName=' + layer.layer_name;
-			var layerResourcesLink = '/gvsigonline/core/download_layer_resources/' + layer.workspace + '/' + layer.layer_name +'/';
+			var layerResourcesLink = '/gvsigonline/services/download_layer_resources/' + layer.workspace + '/' + layer.layer_name +'/';
 			
 			if (viewer.core.conf.user && viewer.core.conf.user.token){
 			    var tk = viewer.core.conf.user.token;
