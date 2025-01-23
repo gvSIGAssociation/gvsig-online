d4dfcb05e34344b51463374255b9341080931d38
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2025-01-23 14:12:07 +0000

    Fix postgres geocoder when same field is used for id and search. Use sqlbuilder to safely encode queries. Fixes #11825
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@8346 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_geocoding/gvsigol_plugin_geocoding/geocoder_postgres.py
+++ plugin_geocoding/gvsigol_plugin_geocoding/geocoder_postgres.py
@@ -32,6 +32,8 @@ import json, requests
 import logging
 
 import psycopg2
+from psycopg2 import sql
+from psycopg2.extensions import quote_ident
 from time import time
 
 class GeocoderPostgres():
@@ -56,8 +58,51 @@ class GeocoderPostgres():
     def is_unique_instance(self):
         return False
 
+    def _set_fields_and_srs(self, provider, introspect_conn):
+        #geom_column_literal = sql.Literal(quote_ident(provider.dbfieldGeom, introspect_conn.conn))
+        geom_column_identifier = sql.Identifier(provider.dbfieldGeom)
+        if provider.dbfield == provider.dbfieldId:
+            if provider.dbfieldId != 'id':
+                alias = 'id'
+            else:
+                alias = 'idcol'
+        else:
+            alias = provider.dbfieldId
+        idfield_sql = sql.SQL('{idfield} as {alias}').format(idfield=sql.Identifier(provider.dbfieldId), alias=sql.Identifier(alias))
+        
+        fields = [
+            #sql.SQL('{dbfield}').format(dbfield=sql.Identifier(provider.dbfield)),
+            sql.Identifier(provider.dbfield),
+            idfield_sql
+        ]
+        fields_with_geom = fields + [
+            sql.SQL('st_transform(ST_PointOnSurface({geom_column}), 4326) AS P').format(geom_column=geom_column_identifier),
+            sql.SQL('st_asgeojson(st_transform({geom_column}, 4326)) as G').format(geom_column=geom_column_identifier)
+        ]
+        """
+            'st_transform(ST_PointOnSurface(' + provider.dbfieldGeom + '), 4326) AS P,', 'st_asgeojson(st_transform(' + provider.dbfieldGeom + ', 4326)) as G ']
+        provider.fieldsWithGeom = (provider.dbfield + ', ' + provider.dbfieldId + ', ' +
+                           'st_transform(ST_PointOnSurface(' + provider.dbfieldGeom + '), 4326) AS P,' +
+                           'st_asgeojson(st_transform(' + provider.dbfieldGeom + ', 4326)) as G ')
+        provider.fields = (provider.dbfield + ', ' + provider.dbfieldId )
+        """
+        
+        fields_sql = sql.SQL(", ").join(fields)
+        provider.fields = fields_sql.as_string(introspect_conn.conn)
+        fields_with_geom_sql = sql.SQL(", ").join(fields_with_geom)
+        provider.fieldsWithGeom = fields_with_geom_sql.as_string(introspect_conn.conn)
+        field_info = introspect_conn.get_geometry_column_info(provider.dbtable, provider.dbfieldGeom, provider.dbschema)
+        provider.srs = field_info[0][4]
 
-    def set_database_config(self,provider):
+    def _set_geocode_query(self, provider):
+        provider.geocoder_query = sql.SQL("""SELECT {fields} FROM {schema}.{table} WHERE immutable_unaccent({dbfield}) %% %s  ORDER BY SIMILARITY( {dbfield}, %s) DESC LIMIT 10""").format(
+                fields = sql.SQL(provider.fields),
+                schema = sql.Identifier(provider.dbschema),
+                table = sql.Identifier(provider.dbtable),
+                dbfield = sql.Identifier(provider.dbfield)
+            )
+
+    def set_database_config(self, provider):
         params = json.loads(provider.params)
 
         datastore_id = params["datastore_id"]
@@ -76,17 +121,10 @@ class GeocoderPostgres():
         provider.dbfieldId = params['id_field']
         provider.dbfieldGeom = params['geom_field']
         provider.dbschema = datastore_params['schema']
-        provider.fieldsWithGeom = (provider.dbfield + ', ' + provider.dbfieldId + ', ' +
-                           'st_transform(ST_PointOnSurface(' + provider.dbfieldGeom + '), 4326) AS P,' +
-                           'st_asgeojson(st_transform(' + provider.dbfieldGeom + ', 4326)) as G ')
-        provider.fields = (provider.dbfield + ', ' + provider.dbfieldId )
-
-        i = Introspect(database=dbname, host=dbhost, port=dbport, user=dbuser, password=dbpassword)
-        field_info = i.get_geometry_column_info(provider.dbtable, provider.dbfieldGeom, provider.dbschema)
-        i.close()
-        
         try:
-            provider.srs = field_info[0][4]
+            with Introspect(database=dbname, host=dbhost, port=dbport, user=dbuser, password=dbpassword) as i:
+                self._set_fields_and_srs(provider, i)
+            self._set_geocode_query(provider)
             provider.connection = psycopg2.connect("host=" + dbhost +" port=" + dbport +" dbname=" + dbname +" user=" + dbuser +" password="+ dbpassword);
             provider.connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
             print("Connect ... ")
@@ -116,16 +154,20 @@ class GeocoderPostgres():
         }
         sanitized_q = query.replace("'", " ")
 
-        # TODO: NO LLAMAR A UNA URL, BUSCAR EN POSTGRES Y DEVOLVER UN JSON
         resul = []
         for provider in self.providers:
+            """
             auxSql =  " '%s' ORDER BY SIMILARITY( %s, '%s') DESC LIMIT 10" % (sanitized_q, provider.dbfield, sanitized_q)
             sqlQuery = ("SELECT " + provider.fields +  " FROM " + provider.dbschema + "." + 
                         provider.dbtable + " WHERE immutable_unaccent(" + provider.dbfield + ") % " + 
                         auxSql)
             # print 'SQL:' + sqlQuery            
-            t1 = time()              
             provider.cursor.execute(sqlQuery)        
+            """
+            
+            t1 = time()
+            #print(provider.geocoder_query.as_string(provider.cursor))
+            provider.cursor.execute(provider.geocoder_query, (query, query))
             rows = provider.cursor.fetchall()
             for r in rows:                
                 a = {
@@ -137,11 +179,11 @@ class GeocoderPostgres():
                     'srs': 'EPSG:4326'
                 }
                 resul.append(a)
-            t2 = time()
-            print('T geocoder postgres ' + provider.dbtable + ':', (t2-t1)*1000, ' msecs')
             
-
-        return resul;
+            if core_settings.DEBUG:
+                t2 = time()
+                print('T geocoder postgres ' + provider.dbtable + ':', (t2-t1)*1000, ' msecs')
+        return resul
 
     def find(self, address_str, exactly_one):
         """
@@ -162,6 +204,7 @@ class GeocoderPostgres():
                 sqlQuery = ("SELECT " + provider.fields +  ", st_x(P) as lng, st_y(P) as lat, G FROM \n" +
                              "(SELECT " +  provider.fieldsWithGeom + " FROM " + provider.dbschema + "." + 
                              provider.dbtable + " WHERE " + provider.dbfield + " like %s ) s"  )
+                print(sqlQuery)
                 provider.cursor.execute(sqlQuery, (the_address, ))        
                 rows = provider.cursor.fetchall()
                 for r in rows:                
@@ -180,7 +223,7 @@ class GeocoderPostgres():
                 print('T geocoder postgres find ' + provider.dbtable + ':', (t2-t1)*1000, ' msecs')
             
 
-        return resul;
+        return resul
 
 
     def reverse(self, coordinate, exactly_one, language):
@@ -212,7 +255,7 @@ class GeocoderPostgres():
             t2 = time()
             print('T geocoder postgres reverse ' + provider.dbtable + ':', (t2-t1)*1000, ' msecs')
 
-        return resul;
+        return resul
 
 
 
