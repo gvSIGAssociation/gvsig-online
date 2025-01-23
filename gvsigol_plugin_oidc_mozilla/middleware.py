bedffa395391e6e6a682d560373fa875e3e112f3
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2025-01-23 13:48:23 +0000

    Exclude AJAX requests from backend token refresh middleware. Fixes #11822
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/plugin_oidc_mozilla@8345 216424d3-d1a8-4ca4-951f-a609719bba38

---
+++ gvsigol_plugin_oidc_mozilla/middleware.py
@@ -0,0 +1,16 @@
+from mozilla_django_oidc.middleware import SessionRefresh
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+class GvsigolSessionRefresh(SessionRefresh):
+    def __init__(self, *args, **kwargs):
+        super(SessionRefresh, self).__init__(*args, **kwargs)
+
+    def process_request(self, request):
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            LOGGER.debug('XMLHttpRequest(s) must be exempt from SessionRefresh')
+            return
+        
+        return super().process_request(request)
+        
