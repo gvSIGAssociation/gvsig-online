f2c7f4164ab47f53cb119b6050231594a2325547
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2025-01-29 12:33:35 +0000

    Tests about tile loading status. Refs: #11847
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol_core@8354 216424d3-d1a8-4ca4-951f-a609719bba38

--- static/js/lib/viewer.js
+++ static/js/lib/viewer.js
@@ -840,14 +840,14 @@ viewer.core = {
 							console.log(image.state);
 							if (exception[0].getAttribute('code') == 'LayerNotDefined') {
 								messageBox.show("error", "The layer does not exists or Geoserver session has expired. Logout from gvSIG Online and login again to reset the session");
-								image.setStage(ol.Tile.ERROR);
+								image.setStage(3); // ol.Tile.ERROR
 							}
 							else if (exception[0].getAttribute('code') == 'TileOutOfRange' || exception[0].getAttribute('code') == 'InvalidDimensionValue') {
 								image.ignoreTileError = true;
-								image.setStage(ol.Tile.EMPTY);
+								image.setStage(4); // ol.Tile.EMPTY
 							}
 							else {
-								image.setStage(ol.Tile.ERROR);
+								image.setStage(3); // ol.Tile.ERROR
 								console.log(exception[0].getAttribute('code'));
 							}
 							console.log(image.state);
