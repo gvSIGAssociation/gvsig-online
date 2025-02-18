ef013383cf60a7a380d1e0bce1e79ba5b0668859
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2025-02-18 22:19:31 +0000

    Don't fail when checking permissions for external layers. Refs: #12161
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@8389 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_services/authorization.py
+++ gvsigol/gvsigol_services/authorization.py
@@ -52,7 +52,11 @@ def get_authz_server_for_layer(layer):
         return instance
     else:
         if not isinstance(layer, Layer):
-            layer = Layer.objects.select_related("datastore__workspace__server").get(id=layer)
+            try:
+                layer = Layer.objects.select_related("datastore__workspace__server").get(id=layer)
+            except:
+                # for external layers
+                layer = Layer.objects.get(id=layer)
         gs = geographic_servers.get_instance().get_server_by_id(layer.datastore.workspace.server.id)
         return gs.getAuthorizationService()
 
