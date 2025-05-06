e0013913c7317922bf733e90ba4baeeb30314c73
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2025-05-06 11:38:39 +0000

    Refactor is_gvsigol_process, is_manage_process and output_gdal_version. Refs: #6435
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol_core@8462 216424d3-d1a8-4ca4-951f-a609719bba38

--- apps.py
+++ apps.py
@@ -12,9 +12,6 @@ class GvsigolCoreConfig(AppConfig):
     label = 'gvsigol_core'
 
     def ready(self):
-        from gvsigol_core.utils import is_gvsigol_process
-        if is_gvsigol_process():
-            self.output_locale_conf()
         from actstream import registry
         registry.register(self.get_model('Project'))
 
@@ -23,28 +20,39 @@ class GvsigolCoreConfig(AppConfig):
             self.config_gdaltools()            
         except:
             pass
-        self.output_gdal_version()
+        from gvsigol_core.utils import is_gvsigol_process, is_shell_process
+        from gvsigol.celery import is_celery_process
+        if is_gvsigol_process() or is_shell_process() or is_celery_process():
+            self.output_locale_conf()
+            self.output_gdal_version()
 
     def output_gdal_version(self):
         try:
-            from gvsigol_core.utils import is_gvsigol_process, is_shell_process    
-            if (is_gvsigol_process() and os.environ.get('RUN_MAIN') == 'true') or is_shell_process():
+            try:
                 import gdaltools
                 ogr = gdaltools.ogr2ogr()
                 (major, minor, patch, prerelease) = ogr.get_version_tuple()
                 print(f"GDAL/OGR version (gdaltools): {major}.{minor}.{patch} {prerelease}")
-                from gvsigol import settings
-                print(f"GDAL_LIBRARY_PATH: {settings.GDAL_LIBRARY_PATH}")
+            except:
+                print("GDAL/OGR version (gdaltools): not available")
+            try:
                 from django.contrib.gis import gdal
                 print(f"GDAL version (django): {gdal.GDAL_VERSION}")
+            except:
+                print("GDAL version (django): not available")
+            try:
+                from gvsigol import settings
+                print(f"GDAL_LIBRARY_PATH: {getattr(settings, 'GDAL_LIBRARY_PATH', '')}")
+            except:
+                print("GDAL_LIBRARY_PATH: not available")
         except:
             pass
 
     def config_gdaltools(self):
+        from gvsigol import settings
         import gdaltools
-        from gvsigol.settings import GDALTOOLS_BASEPATH
-        
-        if GDALTOOLS_BASEPATH and GDALTOOLS_BASEPATH != '':
+        GDALTOOLS_BASEPATH = getattr(settings, 'GDALTOOLS_BASEPATH', '')
+        if GDALTOOLS_BASEPATH:
             gdaltools.Wrapper.BASEPATH = GDALTOOLS_BASEPATH
 
     def output_locale_conf(self):
