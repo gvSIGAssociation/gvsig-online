5a49d4ab0e06eeb60e992e959c2b22a1b7bd5eee
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2025-06-18 08:35:16 +0000

    Don't force session refresh for swagger in debug mode. Refs: #12501
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@8543 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_oidc_mozilla/gvsigol_plugin_oidc_mozilla/middleware.py
+++ plugin_oidc_mozilla/gvsigol_plugin_oidc_mozilla/middleware.py
@@ -1,5 +1,8 @@
 from mozilla_django_oidc.middleware import SessionRefresh
 import logging
+from django.conf import settings
+from django.shortcuts import reverse
+from urllib.parse import urlparse
 
 LOGGER = logging.getLogger(__name__)
 
@@ -11,6 +14,15 @@ class GvsigolSessionRefresh(SessionRefresh):
         if request.headers.get('x-requested-with') == 'XMLHttpRequest':
             LOGGER.debug('XMLHttpRequest(s) must be exempt from SessionRefresh')
             return
+        elif settings.DEBUG and request.headers.get('referer', '').endswith("swagger/"):
+            url = request.headers.get('referer', '')
+            parsed = urlparse(url)
+            baseurl = parsed.scheme + "://" + parsed.netloc
+            if parsed.port:
+                baseurl += ":" + parsed.port
+            if baseurl in settings.ALLOWED_HOST_NAMES and parsed.path == reverse('schema-swagger-ui'):
+                LOGGER.debug('Swagger ajax requests must be exempt from SessionRefresh')
+                return
         
         return super().process_request(request)
         
