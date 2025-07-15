31d89419d77149eeccee76f3c43a118d0faef9a6
mchaveli <mchaveli@216424d3-d1a8-4ca4-951f-a609719bba38>
2025-07-15 14:46:54 +0000

    Cambios archivos documentacion
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk/gvsigol/gvsigol_core@8576 216424d3-d1a8-4ca4-951f-a609719bba38

--- views.py
+++ views.py
@@ -1444,6 +1444,7 @@ def documentation(request):
     lang = request.LANGUAGE_CODE
     base_docs_url = getattr(settings, 'DOCS_PATH', os.path.join(settings.STATIC_ROOT, '../../docs'))
 
+    intro = get_manual(request, 'gvsigol_intro.pdf')
     admin = get_manual(request, 'gvsigol_admin_guide.pdf')
     viewer = get_manual(request, 'gvsigol_user_manual.pdf')
     spaviewer = get_manual(request, 'gvsigolfrontend_manual.pdf')
@@ -1476,6 +1477,8 @@ def documentation(request):
     response = {
         'plugin_manuals': plugin_manuals
     }
+    if intro:
+        response['intro'] = intro
     if admin:
         response['admin'] = admin
     if viewer:
