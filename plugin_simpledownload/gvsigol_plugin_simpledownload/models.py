a949566ad74cbaed9254c78f768843c3b84b299f
lsanjaime <lsanjaime@216424d3-d1a8-4ca4-951f-a609719bba38>
2025-11-04 07:33:42 +0000

    file_configs transformed in list - refs #14671
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@8979 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_simpledownload/gvsigol_plugin_simpledownload/models.py
+++ plugin_simpledownload/gvsigol_plugin_simpledownload/models.py
@@ -9,7 +9,7 @@ class SimpleDownloadConfig(models.Model):
     Configuración del plugin Simple Download por proyecto
     """
     project = models.OneToOneField(Project, on_delete=models.CASCADE, verbose_name="Proyecto")
-    file_configs = JSONField(default=dict, blank=True, verbose_name="Configuración de archivos")
+    file_configs = JSONField(default=list, blank=True, verbose_name="Configuración de archivos")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
@@ -23,8 +23,15 @@ class SimpleDownloadConfig(models.Model):
     
     def get_file_config(self, file_id):
         """Obtener configuración de un archivo específico"""
+        if isinstance(self.file_configs, list):
+            for file_config in self.file_configs:
+                if file_config.get('id') == file_id:
+                    return file_config
+            return {}
         return self.file_configs.get(str(file_id), {})
 
     def get_all_files(self):
         """Obtener todos los archivos ordenados por ID"""
+        if isinstance(self.file_configs, list):
+            return sorted(self.file_configs, key=lambda x: x.get('id', 0))
         return sorted(self.file_configs.items(), key=lambda x: int(x[0]) if x[0].isdigit() else 0)
