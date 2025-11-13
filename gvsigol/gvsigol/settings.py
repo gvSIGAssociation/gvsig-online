75d299bc99fe85f51f9c7ebc617c3abf45775188
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2025-11-13 12:39:18 +0000

    Versión 3.11.1 #14783
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/tags/gvsigol_v3.11.1@9006 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol/settings.py
+++ gvsigol/gvsigol/settings.py
@@ -38,7 +38,7 @@ import json
 import sys
 
 print ("INFO: Ejecutando settings.py !!...........................................")
-GVSIGOL_VERSION = '3.11.1-dev'
+GVSIGOL_VERSION = '3.11.1'
 print("INFO: gvSIG Online version: " + GVSIGOL_VERSION)
 if os.environ.get("DEBUG")=='True':
     print ("INFO: Current environment:")
