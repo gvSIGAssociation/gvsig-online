436497da6dab621b3e2f635a4da133c83e0137f6
respinosa <respinosa@216424d3-d1a8-4ca4-951f-a609719bba38>
2025-11-20 11:46:58 +0000

    Eliminado directorio plugin_catastrors_admin que fue creado por error. refs #14897
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@9024 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_catastrord_admin/gvsigol_plugin_catastrord_admin/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-default_app_config = 'gvsigol_plugin_catastrord_admin.apps.GvsigolPluginCatastrordAdminConfig'
--- plugin_catastrord_admin/gvsigol_plugin_catastrord_admin/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import catastrord_admin
-
-app_name = 'gvsigol_plugin_catastrord_admin'
-
-urlpatterns = [
-    # Vista HTML principal con pestañas
-    path('catastrord/admin/', catastrord_admin, name='catastrord_admin'),
-]
