c74e630562d142fd853be4f3eaf401df7026b14b
jrodrigo <jrodrigo@216424d3-d1a8-4ca4-951f-a609719bba38>
2026-03-09 17:03:57 +0000

    se modifica estilo
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@9385 216424d3-d1a8-4ca4-951f-a609719bba38

--- app_dev/gvsigol_app_dev/views.py
+++ app_dev/gvsigol_app_dev/views.py
@@ -26,6 +26,45 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from gvsigol import settings
 from gvsigol_core.views import default_index
+from gvsigol_core.models import Project, UserHomeOrder
+from urllib.parse import unquote
+import datetime
+import json
+
+
+def _get_public_projects_ordered():
+    """Return public projects list applying the global UserHomeOrder if set."""
+    query = (
+        Project.objects.filter(is_public=True, expiration_date__gte=datetime.datetime.now()) |
+        Project.objects.filter(is_public=True, expiration_date=None)
+    )
+    public_projects = []
+    for p in query.order_by('title'):
+        public_projects.append({
+            'id': p.id,
+            'title': p.title,
+            'description': p.description or '',
+            'image': unquote(p.image_url),
+            'url': p.url,
+        })
+
+    global_order = UserHomeOrder.objects.filter(is_global=True).first()
+    if global_order and global_order.order_type == UserHomeOrder.ORDER_MANUAL and global_order.order_data:
+        try:
+            order_list = json.loads(global_order.order_data)
+            lookup = {p['id']: p for p in public_projects}
+            seen = set()
+            ordered = []
+            for entry in order_list:
+                if entry.get('type') == 'public' and entry['id'] in lookup and entry['id'] not in seen:
+                    ordered.append(lookup[entry['id']])
+                    seen.add(entry['id'])
+            ordered += [p for p in public_projects if p['id'] not in seen]
+            public_projects = ordered
+        except Exception:
+            pass
+
+    return public_projects
 
 
 def index(request):
@@ -38,5 +77,8 @@ def index(request):
     if 'gvsigol_plugin_sync' in settings.INSTALLED_APPS:
         from gvsigol_plugin_sync import settings as sync_settings
         resp['GVSIGOL_APP_DOWNLOAD_LINK'] = sync_settings.GVSIGOL_APP_DOWNLOAD_LINK
+
+    resp['public_projects'] = _get_public_projects_ordered()
+
     return default_index(request, response=resp)
 
