8b1fea41dd3f945b21565f3eac04de0a23c1bf0e
lsanjaime <lsanjaime@216424d3-d1a8-4ca4-951f-a609719bba38>
2026-03-17 15:17:26 +0000

    Refs #15845 - #16097: when cached and time_enabled no use WMS to call geowebcache
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/branches/plugin_projectapi_refs15845@9467 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol_plugin_projectapi/infoserializer.py
+++ gvsigol_plugin_projectapi/infoserializer.py
@@ -365,7 +365,7 @@ class LayerSerializer(serializers.ModelSerializer):
             workspace = obj.datastore.workspace
             url = workspace.wms_endpoint.replace(settings.BASE_URL, '')
             if obj.cached:
-                if not obj.time_enabled and workspace.wmts_endpoint and len(workspace.wmts_endpoint) > 0:
+                if workspace.wmts_endpoint and workspace.wmts_endpoint.__len__() > 0:
                     url = workspace.wmts_endpoint.replace(settings.BASE_URL, '')
                 else:
                     url = workspace.cache_endpoint.replace(settings.BASE_URL, '')
