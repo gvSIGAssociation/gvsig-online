20736325b0f97e0ecf0fc960e5b58e60823a5523
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2026-03-25 16:56:21 +0000

    Fix duplicated url names: Adapt code to be compatible with Django 2.2 AND 4.x. Refs: #16320
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@9486 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_statistics/urls.py
+++ gvsigol/gvsigol_statistics/urls.py
@@ -5,7 +5,7 @@ app_name="gvsigol_statistics"
 urlpatterns = [
     path('activity/', include('actstream.urls')),
     path('get_registered_actions/<plugin_name>/<action_name>/', views.get_registered_actions, name='get_registered_actions'),
-    path('register_action/', views.register_action, name='register_action'),
+    path('register_action/', views.register_action, name='statistics_register_action'),
 
     path('get-targets-from-content-type/', views.get_targets_from_content_type, name='get_targets_from_content_type'),
 
--- plugin_alfresco/gvsigol_plugin_alfresco/urls.py
+++ plugin_alfresco/gvsigol_plugin_alfresco/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from gvsigol_plugin_alfresco import views
 
 urlpatterns = [
-    path('alfresco/get_sites/', views.get_sites, name='get_sites'),
-    path('alfresco/get_folder_content/', views.get_folder_content, name='get_folder_content'),
-    path('alfresco/save_resource/', views.save_resource, name='save_resource'),
-    path('alfresco/update_resource/', views.update_resource, name='update_resource'),
-    path('alfresco/delete_resource/', views.delete_resource, name='delete_resource'),
+    path('alfresco/get_sites/', views.get_sites, name='alfresco_get_sites'),
+    path('alfresco/get_folder_content/', views.get_folder_content, name='alfresco_get_folder_content'),
+    path('alfresco/save_resource/', views.save_resource, name='alfresco_save_resource'),
+    path('alfresco/update_resource/', views.update_resource, name='alfresco_update_resource'),
+    path('alfresco/delete_resource/', views.delete_resource, name='alfresco_delete_resource'),
 ]
