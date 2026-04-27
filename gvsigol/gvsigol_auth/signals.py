68570c3b131d91ece606426aa784c3f362499d11
cmartinez <cmartinez@216424d3-d1a8-4ca4-951f-a609719bba38>
2026-04-27 12:04:09 +0000

    Signals: Adapt code to be compatible with Django 2.2 AND 4.x. Refs: #16320
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@9595 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_auth/signals.py
+++ gvsigol/gvsigol_auth/signals.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.dispatch import Signal
 
-role_added = Signal(providing_args=["role"])
-role_deleted = Signal(providing_args=["role"])
+role_added = Signal()  # providing_args=["role"]
+role_deleted = Signal()  # providing_args=["role"]
 
-user_updated = Signal(providing_args=["username", "user_obj", "user_dict", "roles"])
-user_roles_updated = Signal(providing_args=["username", "roles"])
-user_deleted = Signal(providing_args=["username"])
+user_updated = Signal()  # providing_args=["username", "user_obj", "user_dict", "roles"]
+user_roles_updated = Signal()  # providing_args=["username", "roles"]
+user_deleted = Signal()  # providing_args=["username"]
