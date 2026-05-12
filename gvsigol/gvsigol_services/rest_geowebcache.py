dda533b09d0f63645f89c91aa29ec3f8fe3f6690
lsanjaime <lsanjaime@216424d3-d1a8-4ca4-951f-a609719bba38>
2026-05-12 12:45:31 +0000

    refs #15939: wmts_options added to cached ext WMS
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@9659 216424d3-d1a8-4ca4-951f-a609719bba38

--- gvsigol/gvsigol_services/rest_geowebcache.py
+++ gvsigol/gvsigol_services/rest_geowebcache.py
@@ -24,6 +24,9 @@
 from gvsigol import settings
 import requests
 import json
+import logging
+
+logger = logging.getLogger(__name__)
 
 class RequestError(Exception):
     def __init__(self, status_code=-1, server_message=""):
@@ -112,10 +115,22 @@ class APIGeoWebCache():
             layer_name = layer.name
             external_params = json.loads(layer.external_params)
             wms_layers = external_params.get('layers')
-            if external_params.get('get_map_url'):
-                url = external_params.get('get_map_url')
-            else:
-                url = external_params
+
+            url = (external_params.get('get_map_url') or external_params.get('url') or '').strip()
+            if not url:
+                logger.error(
+                    "GWC add_layer: external layer %s has no get_map_url or url",
+                    getattr(layer, "name", layer),
+                )
+                raise FailedRequestError(
+                    -1,
+                    "External cached layer needs get_map_url or url (WMS GetMap endpoint) to register in GeoWebCache.",
+                )
+            if not wms_layers:
+                raise FailedRequestError(
+                    -1,
+                    "External cached layer needs 'layers' (remote WMS layer name)",
+                )
         else:
             layer_name = ws + ":" + layer.name
             wms_layers = layer_name
