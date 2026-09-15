f7b4e0cb3756417f7e9d6ca17de4549adf200928
carlesmarti <carlesmarti@216424d3-d1a8-4ca4-951f-a609719bba38>
2026-09-15 09:14:34 +0000

    refs #18035
    git-svn-id: https://devel.scolab.eu/gvsig-online/gvsigonline_v2/trunk@9998 216424d3-d1a8-4ca4-951f-a609719bba38

--- plugin_projectapi/gvsigol_plugin_projectapi/infoserializer.py
+++ plugin_projectapi/gvsigol_plugin_projectapi/infoserializer.py
@@ -238,6 +238,10 @@ class LayerSerializer(serializers.ModelSerializer):
                         field['dimension'] = field_conf['dimension']
                     if field_conf.get('calculated_formula'):
                         field['calculated_formula'] = field_conf['calculated_formula']
+                    if field_conf.get('calculated_sources'):
+                        field['calculated_sources'] = field_conf['calculated_sources']
+                    if field_conf.get('calculated_base'):
+                        field['calculated_base'] = field_conf['calculated_base']
                     if field_conf.get('calculated_sources') is not None:
                         field['calculated_sources'] = field_conf['calculated_sources']
                     if field_conf.get('calculated_base'):
